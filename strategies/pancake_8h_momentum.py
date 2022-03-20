9348d61f885397aa72bcae41efef7954bf863bb2
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-03-20 15:46:02 +0100

    Remove unit test checks

--- strategies/pancake_8h_momentum.py
+++ strategies/pancake_8h_momentum.py
@@ -326,9 +326,8 @@ def strategy_factory(
         # https://www.python.org/dev/peps/pep-3102/
         raise TypeError("Only keyword arguments accepted")
 
-    assert isinstance(execution_model, UniswapV2ExecutionModel), f"This strategy is compatible only with UniswapV2ExecutionModel, got {execution_model}"
-
-    assert execution_model.chain_id == 1337, f"This strategy is hardcoded to ganache-cli test chain, got chain {execution_model.chain_id}"
+    # assert isinstance(execution_model, UniswapV2ExecutionModel), f"This strategy is compatible only with UniswapV2ExecutionModel, got {execution_model}"
+    # assert execution_model.chain_id == 1337, f"This strategy is hardcoded to ganache-cli test chain, got chain {execution_model.chain_id}"
 
     universe_model = OurUniverseModel(client, timed_task_context_manager)
 
