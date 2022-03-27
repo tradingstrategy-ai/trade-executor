ce954ef8c6c4f48d546c7bfab58748eea6d5682b
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-03-27 14:02:31 +0200

    Turn on Ganache logging to figure out WTF is its issue

--- tests/test_pancake_8h_momentum.py
+++ tests/test_pancake_8h_momentum.py
@@ -73,7 +73,7 @@ def ganache_bnb_chain_fork(logger, large_busd_holder) -> str:
             block_time=1,  # Insta mining cannot be done in this test
             evm_version="berlin",  # BSC is not yet London compatible?
             unlocked_addresses=[large_busd_holder],  # Unlock WBNB stealing
-            quiet=True,  # Otherwise the Ganache output is millions lines of long
+            quiet=False,  # Otherwise the Ganache output is millions lines of long
         )
         yield launch.json_rpc_url
         # Wind down Ganache process after the test is complete
