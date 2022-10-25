381ac9bf7d06fc97f3760f80b603aa4b648f116d
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-10-25 11:46:43 +0200

    More pyramid to optional dependencies

--- tests/test_optional_dependencies.py
+++ tests/test_optional_dependencies.py
@@ -1,8 +1,9 @@
-import pytest
-
 
 def test_optional_dependencies():
-    """See that we can import the library without optional dependencies."""
+    """See that we can import the library without optional dependencies.
+
+    See .github/workflows/client-side.yml
+    """
 
     from tradeexecutor.state import state
     from tradeexecutor.statistics import core
