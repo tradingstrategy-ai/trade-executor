1fb689ee627dc9d18a59e7c5a46d937b46cea0b3
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-11-27 14:20:13 +0100

    Clean up the comment

--- tradeexecutor/state/validator.py
+++ tradeexecutor/state/validator.py
@@ -1,14 +1,15 @@
-"""State extra validation.
+"""Stateful data validation.
 
-Try avoid non-JSON supported types to slipping into to the state.
-We have some common culprints we want to catch.
+Avoid non-JSON supported types to slipping into to the state.
+We have some common culprits we want to catch.
 
+Any error message contains tree presentation of the state,
+so you can easily locate any values that are bad, unlike with :py:mod:`json`.
 """
 import datetime
 from decimal import Decimal
 from enum import Enum
 from types import NoneType
-from typing import Any
 
 import pandas as pd
 import numpy as np
@@ -44,16 +45,23 @@ BAD_VALUE_TYPES = (
 )
 
 
-def validate_state_value(name: str | int, val: Any):
-    if not isinstance(val, ALLOWED_VALUE_TYPES):
-        raise BadStateData(f"{name}: {val} ({type(val)} - value type is not in supported serialisable types")
+def validate_state_value(name: str | int, val: object):
+    """Check the state value against our whitelist and blacklist."""
 
     if isinstance(val, BAD_VALUE_TYPES):
         raise BadStateData(f"{name}: {val} ({type(val)} - blacklisted value type")
 
+    if not isinstance(val, ALLOWED_VALUE_TYPES):
+        raise BadStateData(f"{name}: {val} ({type(val)} - value type is not in supported serialisable types")
 
-def walk(name, val):
-    """Raise hierarchical exceptions to locate the bad key-value pair in nested data."""
+
+def walk(name: str | int, val: dict | list | object):
+    """Raise hierarchical exceptions to locate the bad key-value pair in nested data.
+
+    :raise BadStateData:
+        In the case we have sneaked something into the state
+        that does not belong there.
+    """
     try:
         if isinstance(val, dict):
             for k, v in val.items():
@@ -68,6 +76,14 @@ def walk(name, val):
 
 
 def validate_nested_state_dict(d: dict | list | object):
+    """Validate state as serialised to a dictionary tree by dataclasses_json.
+
+    See `to_dict` in `dataclass_json`.
+
+    :raise BadStateData:
+        In the case we have sneaked something into the state
+        that does not belong there.
+    """
     walk("state", d)
 
 
@@ -75,6 +91,10 @@ def validate_state_serialisation(state: State):
     """Check that we can write the state to the disk,
 
     Unlike `json.dump()` gives user friendly error messages.
+
+    :raise BadStateData:
+        In the case we have sneaked something into the state
+        that does not belong there.
     """
     d = state.to_dict()
     validate_nested_state_dict(d)
