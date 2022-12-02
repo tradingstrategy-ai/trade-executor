82bc9ef6a78c34750a8fa452582e10830223f6f6
alex urban <alexurban12@gmail.com>
2022-12-02 16:40:44 +0200

    clean comments

--- strategies/live/avax_usdc.py
+++ strategies/live/avax_usdc.py
@@ -22,44 +22,27 @@ from tradingstrategy.client import Client
 
 from tradeexecutor.strategy.universe_model import UniverseOptions
 
-# Tell what trade execution engine version this strategy needs to use
 # NOTE: this setting has currently no effect
 TRADING_STRATEGY_ENGINE_VERSION = "0.1"
 
-# What kind of strategy we are running.
-# This tells we are going to use
 # NOTE: this setting has currently no effect
 TRADING_STRATEGY_TYPE = StrategyType.managed_positions
 
-# How our trades are routed.
-# PancakeSwap basic routing supports two way trades with BUSD
-# and three way trades with BUSD-BNB hop.
-TRADE_ROUTING = TradeRouting.quickswap_usdc
-# How often the strategy performs the decide_trades cycle.
-# We do it for every 4h.
+TRADE_ROUTING = TradeRouting.trader_joe_usdc
+
 TRADING_STRATEGY_CYCLE = CycleDuration.cycle_1h
 
-# Strategy keeps its cash in USDC
 RESERVE_CURRENCY = ReserveCurrency.usdc
 
-# Time bucket for our candles
 CANDLE_TIME_BUCKET = TimeBucket.h1
 
-# Which chain we are trading
-CHAIN_ID = ChainId.polygon
+CHAIN_ID = ChainId.avalanche
 
-# Which exchange we are trading on.
-EXCHANGE_SLUG = "quickswap"
+EXCHANGE_SLUG = "trader-joe"
 
-# Which trading pair we are trading
-TRADING_PAIR = ("WMATIC", "USDC")
+TRADING_PAIR = ("WAVAX", "USDC")
 
-# How much of the cash to put on a single trade
 POSITION_SIZE = 0.70
-
-#
-# Strategy thinking specific parameter
-#
 
 BATCH_SIZE = 90
 
@@ -67,25 +50,18 @@ SLOW_EMA_CANDLE_COUNT = 10
 FAST_EMA_CANDLE_COUNT = 3
 
 
-# Range of backtesting and synthetic data generation.
-# Because we are using synthetic data actual dates do not really matter -
-# only the duration
-
 START_AT = datetime.datetime(2022, 1, 1)
 
 END_AT = datetime.datetime(2022, 10, 18)
 
-# Start with 10,000 USD
 INITIAL_DEPOSIT = 10_000
 
-# If the price drops 0.7% we trigger a stop loss
 STOP_LOSS_PCT = 0.993
 
 STOP_LOSS_TIME_BUCKET = TimeBucket.m15
 
 
 logger = logging.getLogger(__name__)
-
 
 def decide_trades(
         timestamp: pd.Timestamp,
@@ -128,23 +104,14 @@ def decide_trades(
         The trades can be generated using `position_manager` but strategy could also hand craft its trades.
     """
 
-    # The pair we are trading
     pair = universe.pairs.get_single()
 
-    # How much cash we have in the hand
     cash = state.portfolio.get_current_cash()
 
-    # Get OHLCV candles for our trading pair as Pandas Dataframe.
-    # We could have candles for multiple trading pairs in a different strategy,
-    # but this strategy only operates on single pair candle.
-    # We also limit our sample size to N latest candles to speed up calculations.
     candles: pd.DataFrame = universe.candles.get_single_pair_data(timestamp, sample_count=BATCH_SIZE)
 
-    # We have data for open, high, close, etc.
-    # We only operate using candle close values in this strategy.
     close_prices = candles["close"]
 
-    # Calculate exponential moving averages based on slow and fast sample numbers.
     slow_ema_series = ema(close_prices, length=SLOW_EMA_CANDLE_COUNT)
     fast_ema_series = ema(close_prices, length=FAST_EMA_CANDLE_COUNT)
 
@@ -180,8 +147,6 @@ def decide_trades(
 
     trades = []
 
-    # Create a position manager helper class that allows us easily to create
-    # opening/closing trades for different positions
     position_manager = PositionManager(timestamp, universe, state, pricing_model)
 
     if not position_manager.is_any_open():
