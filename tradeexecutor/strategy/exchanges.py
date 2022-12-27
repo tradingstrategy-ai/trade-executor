51e46905763cf4194f251d2925af68b8e0985698
alex urban <alexurban12@gmail.com>
2022-12-27 12:33:10 +0200

    fix exchangeSlug  enum

--- tradeexecutor/strategy/exchanges.py
+++ tradeexecutor/strategy/exchanges.py
@@ -6,11 +6,14 @@
 """
 import enum
 
+from tradingstrategy.chain import ChainId
 from tradeexecutor.utils.timer import timed_task
 
-
 class ExchangeSlug(enum.Enum):
-    """Different exchanges that can be used for the strategy."""
+    """Different exchanges that can be used for the strategy.
+    
+    TODO add support in codebase for using this enum
+    """
 
     pancakeswap_v2 = "pancakeswap-v2"
 
@@ -27,12 +30,33 @@ class ExchangeSlug(enum.Enum):
         
         This method needs to be kept updated with the latest uniswap v3 like exchanges
         """
-        return {self.uniswap_v3.value}
+        return {self.uniswap_v3}
 
     def is_uniswap_v2_like(self) -> bool:
         """Returns a true if value is uniswap v2 like, false if uniswap v3 like"""
-        return self.value not in self.get_uniswap_v3_like()
+        return self not in self.get_uniswap_v3_like()
 
     def is_uniswap_v3_like(self) -> bool:
         """Returns a true if value is uniswap v3 like, false if uniswap v2 like"""
-        return self.value in self.get_uniswap_v3_like()
+        return self in self.get_uniswap_v3_like()
+    
+    def get_chain_id(self) -> ChainId:
+        """Gets the chain id for the relevant exchange.
+        
+        TODO use to validate that user has inputted correct chainid/exchange.
+         
+        Note that we can't automatically get chain id without user input because some exchanges
+        such as uniswap operate on multiple blockchains.
+        """
+        if self in [self.uniswap_v2, self.uniswap_v3]:
+            return {ChainId.ethereum, ChainId.polygon}
+        elif self in [self.pancakeswap_v2]:
+            return {ChainId.bsc}
+        elif self in [self.quickswap]:
+            return {ChainId.polygon}
+        elif self in [self.trader_joe]:
+            return {ChainId.avalanche}
+        else:
+            raise TypeError()
+
+
