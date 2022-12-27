cf625df7747bf62509e7bfc24b2547c63de1388d
alex urban <alexurban12@gmail.com>
2022-12-27 10:39:49 +0200

    update and fix exchanges enum

--- tradeexecutor/strategy/exchanges.py
+++ tradeexecutor/strategy/exchanges.py
@@ -9,10 +9,9 @@ import enum
 from tradeexecutor.utils.timer import timed_task
 
 
-class Exchange(enum.Enum):
+class ExchangeSlug(enum.Enum):
     """Different exchanges that can be used for the strategy."""
 
-    #: TODO documentation comments
     pancakeswap_v2 = "pancakeswap-v2"
 
     uniswap_v2 = "uniswap-v2"
@@ -23,8 +22,17 @@ class Exchange(enum.Enum):
 
     quickswap = "quickswap"
 
+    def get_uniswap_v3_like(self) -> set:
+        """Returns a set of all uniswap v3 like exchanges.
+        
+        This method needs to be kept updated with the latest uniswap v3 like exchanges
+        """
+        return {self.uniswap_v3.value}
+
     def is_uniswap_v2_like(self) -> bool:
-        return True if self.value != self.uniswap_v3 else False
+        """Returns a true if value is uniswap v2 like, false if uniswap v3 like"""
+        return self.value not in self.get_uniswap_v3_like()
 
     def is_uniswap_v3_like(self) -> bool:
-        return True if self.value == self.uniswap_v3 else False
+        """Returns a true if value is uniswap v3 like, false if uniswap v2 like"""
+        return self.value in self.get_uniswap_v3_like()
