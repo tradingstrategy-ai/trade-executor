efc1833f7278e043a7d6e28fe69118e16dde7ecd
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-01-12 19:24:55 +0200

    Allow non-even hours in backtest valuation

--- tradeexecutor/backtest/backtest_valuation.py
+++ tradeexecutor/backtest/backtest_valuation.py
@@ -9,7 +9,10 @@ from tradeexecutor.strategy.valuation import ValuationModel
 
 
 class BacktestValuationModel(ValuationModel):
-    """Re-value assets based on their on-chain backtest dataset price."""
+    """Re-value assets based on their on-chain backtest dataset price.
+
+    Each asset is valued at its market sell price estimation.
+    """
 
     def __init__(self, pricing_model: BacktestSimplePricingModel):
         assert pricing_model, "pricing_model missing"
@@ -20,15 +23,14 @@ class BacktestValuationModel(ValuationModel):
                  position: TradingPosition) -> Tuple[datetime.datetime, USDollarAmount]:
 
         assert isinstance(ts, datetime.datetime)
-        assert ts.minute == 0, f"Timestamp sanity check failed {ts}"
-        assert ts.second == 0, f"Timestamp sanity check failed {ts}"
+        assert ts.second == 0, f"Timestamp sanity check failed, does not have even seconds: {ts}"
 
         pair = position.pair
 
         assert position.is_long(), "Short not supported"
         quantity = position.get_quantity()
-        price_structure = self.pricing_model.get_sell_price(ts, pair, quantity)
-        return ts, float(price_structure.price)
+        price = self.pricing_model.get_sell_price(ts, pair, quantity)
+        return ts, float(price)
 
 
 def backtest_valuation_factory(pricing_model):
