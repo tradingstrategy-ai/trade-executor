611c855d0cf4f6fb4db7b87fb7a6215e96d71de0
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-02-10 21:02:52 +0100

    Even more fee fixes

--- tests/test_execute_trade_uniswap.py
+++ tests/test_execute_trade_uniswap.py
@@ -148,12 +148,12 @@ def weth_usdc_uniswap_trading_pair(web3, deployer, uniswap_v2, weth_token, usdc_
 
 @pytest.fixture
 def weth_usdc_pair(uniswap_v2, weth_usdc_uniswap_trading_pair, asset_usdc, asset_weth) -> TradingPairIdentifier:
-    return TradingPairIdentifier(asset_weth, asset_usdc, weth_usdc_uniswap_trading_pair, uniswap_v2.factory.address)
+    return TradingPairIdentifier(asset_weth, asset_usdc, weth_usdc_uniswap_trading_pair, uniswap_v2.factory.address, fee=0)
 
 
 @pytest.fixture
 def aave_usdc_pair(uniswap_v2, aave_usdc_uniswap_trading_pair, asset_usdc, asset_aave) -> TradingPairIdentifier:
-    return TradingPairIdentifier(asset_aave, asset_usdc, aave_usdc_uniswap_trading_pair, uniswap_v2.factory.address)
+    return TradingPairIdentifier(asset_aave, asset_usdc, aave_usdc_uniswap_trading_pair, uniswap_v2.factory.address, fee=0)
 
 
 @pytest.fixture
