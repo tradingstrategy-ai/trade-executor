11b0d8543551e3edd8e23798cc211da2146860cc
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-02-20 12:30:58 +0100

    Add support for timedelta serialisation

--- tradeexecutor/monkeypatch/dataclasses_json.py
+++ tradeexecutor/monkeypatch/dataclasses_json.py
@@ -1,10 +1,37 @@
 """Fix dataclasses_json to always serialised and deserialise dates in the timezone-naive format."""
-from datetime import datetime
+import json
+from datetime import datetime, timedelta
 from datetime import timezone
 from decimal import Decimal
+from enum import Enum
+from typing import Collection, Mapping
 from uuid import UUID
 
 from dataclasses_json import core
+
+
+
+def _patched_default(self, o) -> core.Json:
+    result: core.Json
+    if core._isinstance_safe(o, Collection):
+        if core._isinstance_safe(o, Mapping):
+            result = dict(o)
+        else:
+            result = list(o)
+    elif core._isinstance_safe(o, datetime):
+        result = o.timestamp()
+    # Patch timedelta support
+    elif core._isinstance_safe(o, timedelta):
+        result = o.total_seconds()
+    elif core._isinstance_safe(o, UUID):
+        result = str(o)
+    elif core._isinstance_safe(o, Enum):
+        result = o.value
+    elif core._isinstance_safe(o, Decimal):
+        result = str(o)
+    else:
+        result = json.JSONEncoder.default(self, o)
+    return result
 
 
 def _patched_support_extended_types(field_type, field_value):
@@ -15,6 +42,14 @@ def _patched_support_extended_types(field_type, field_value):
             # Fixed here
             # tz = datetime.now(timezone.utc).astimezone().tzinfo
             res = datetime.fromtimestamp(field_value, tz=None)
+    # Add timedelta support
+    elif core._issubclass_safe(field_type, timedelta):
+        if isinstance(field_value, timedelta):
+            res = field_value
+        else:
+            # Fixed here
+            # tz = datetime.now(timezone.utc).astimezone().tzinfo
+            res = timedelta(seconds=field_value)
     elif core._issubclass_safe(field_type, Decimal):
         res = (field_value
                if isinstance(field_value, Decimal)
@@ -31,4 +66,6 @@ def _patched_support_extended_types(field_type, field_value):
 def patch_dataclasses_json():
     if core._support_extended_types != _patched_support_extended_types:
         core._support_extended_types = _patched_support_extended_types
+
+    core._ExtendedEncoder.default = _patched_default
 
