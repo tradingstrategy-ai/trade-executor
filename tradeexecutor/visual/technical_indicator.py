fbdf4ddcec9cdaeef5273c639a481c0ca9f0c103
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-04-13 10:47:23 +0200

    move code

--- tradeexecutor/visual/technical_indicator.py
+++ tradeexecutor/visual/technical_indicator.py
@@ -71,31 +71,6 @@ def overlay_all_technical_indicators(
         else:
             raise ValueError(f"Unknown plot kind: {plot.plot_kind}")
 
-
-        
-
-def _get_initial_row(volume_bar_mode: VolumeBarMode):
-    """Get first row for plot based on volume bar mode."""
-    if volume_bar_mode in {VolumeBarMode.hidden, VolumeBarMode.overlay}:
-        cur_row = 1
-    elif volume_bar_mode == VolumeBarMode.separate:
-        cur_row = 2
-    else:
-        raise ValueError("Unknown volume bar mode: %s" % volume_bar_mode)
-    return cur_row
-
-def _get_start_and_end(
-    start_at: pd.Timestamp | None, 
-    end_at: pd.Timestamp | None, 
-    plot: Plot,
-):
-    """Get first and last entry from plot if start_at and end_at are not set."""
-    if not start_at and end_at:
-        start_at = plot.get_first_entry()[0]
-        end_at = plot.get_last_entry()[0]
-    return start_at,end_at
-
-
 def visualise_technical_indicator(
         plot: Plot,
         start_at: Optional[pd.Timestamp] = None,
@@ -121,6 +96,55 @@ def visualise_technical_indicator(
         return None
     
     return _get_plot(df, plot)
+
+def export_plot_as_dataframe(
+        plot: Plot,
+        start_at: Optional[pd.Timestamp] = None,
+        end_at: Optional[pd.Timestamp] = None,
+) -> pd.DataFrame:
+    """Convert visualisation state to Plotly friendly df.
+
+    :param plot:
+        Internal Plot object from the strategy state.
+
+    :param start_at:
+        Crop range
+
+    :param end_at:
+        Crop range
+    """
+    data = []
+    for time, value in plot.points.items():
+        time = pd.to_datetime(time, unit='s')
+
+        if start_at or end_at:
+            if time < start_at or time > end_at:
+                continue
+
+        data.append({
+            "timestamp": time,
+            "value": value,
+        })
+
+    # set_index fails if the frame is empty
+    if len(data) == 0:
+        return pd.DataFrame()
+
+    # Convert timestamp to pd.Timestamp column
+    df = pd.DataFrame(data)
+    df = df.set_index(pd.DatetimeIndex(df["timestamp"]))
+    return df
+
+
+def _get_initial_row(volume_bar_mode: VolumeBarMode):
+    """Get first row for plot based on volume bar mode."""
+    if volume_bar_mode in {VolumeBarMode.hidden, VolumeBarMode.overlay}:
+        cur_row = 1
+    elif volume_bar_mode == VolumeBarMode.separate:
+        cur_row = 2
+    else:
+        raise ValueError("Unknown volume bar mode: %s" % volume_bar_mode)
+    return cur_row
 
 def _get_plot(df: pd.DataFrame, plot: Plot):
     """Get plot based on plot shape and plot size."""
@@ -170,42 +194,3 @@ def _get_linear_plot(df: pd.DataFrame, plot: Plot) -> go.Scatter:
             line=dict(color=plot.colour),
             line_shape=plot.plot_shape.value
         )
-
-
-def export_plot_as_dataframe(
-        plot: Plot,
-        start_at: Optional[pd.Timestamp] = None,
-        end_at: Optional[pd.Timestamp] = None,
-) -> pd.DataFrame:
-    """Convert visualisation state to Plotly friendly df.
-
-    :param plot:
-        Internal Plot object from the strategy state.
-
-    :param start_at:
-        Crop range
-
-    :param end_at:
-        Crop range
-    """
-    data = []
-    for time, value in plot.points.items():
-        time = pd.to_datetime(time, unit='s')
-
-        if start_at or end_at:
-            if time < start_at or time > end_at:
-                continue
-
-        data.append({
-            "timestamp": time,
-            "value": value,
-        })
-
-    # set_index fails if the frame is empty
-    if len(data) == 0:
-        return pd.DataFrame()
-
-    # Convert timestamp to pd.Timestamp column
-    df = pd.DataFrame(data)
-    df = df.set_index(pd.DatetimeIndex(df["timestamp"]))
-    return df
