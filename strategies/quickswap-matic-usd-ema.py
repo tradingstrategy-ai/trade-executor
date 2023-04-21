5be944cb6435ddff368ef9e5e360c0c33d1c9b05
--global <--global>
2023-04-21 10:46:24 +0200

    fix #137

--- strategies/quickswap-matic-usd-ema.py
+++ strategies/quickswap-matic-usd-ema.py
@@ -176,6 +176,6 @@ def create_trading_universe(
         TRADING_PAIR[1],
     )
 
-    logger.warning("Universe created, we have %d pairs")
+    logger.warning("Universe created, we have %d pairs", universe.get_pair_count())
 
     return universe
