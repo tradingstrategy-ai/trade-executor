a2c91607f46d49c31ab5e5f5520c11ae3c310a85
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-04-30 16:33:18 +0200

    Add parameterised rows to grid search analysis

--- tradeexecutor/analysis/grid_search.py
+++ tradeexecutor/analysis/grid_search.py
@@ -1,3 +1,5 @@
+"""Grid search result analysis."""
+
 from typing import Iterable
 
 import pandas as pd
@@ -7,11 +9,18 @@ from tradeexecutor.backtest.grid_search import GridSearchResult
 
 def analyse_combination(r: GridSearchResult) -> dict:
     """Create a grid search result table row."""
-    return {
+
+    row = {}
+
+    for param in r.combination.parameters:
+        row[param.name] = param.value
+
+    row.update({
         "Combination": r.combination.get_label(),
         "Annualised profit": r.summary.annualised_return_percent,
         "Max drawdown": r.metrics.loc["Max Drawdown"][0]
-    }
+    })
+    return row
 
 
 def analyse_grid_search_result(results: Iterable[GridSearchResult]) -> pd.DataFrame:
@@ -20,3 +29,19 @@ def analyse_grid_search_result(results: Iterable[GridSearchResult]) -> pd.DataFr
     df = pd.DataFrame(rows)
     df = df.set_index("Combination")
     return df
+
+
+def visualise_heatmap_2d(
+        result: pd.DataFrame,
+        metric: str,
+        parameter_1: str,
+        parameter_2: str,
+):
+    """Draw a heatmap square comparing two different parameters.
+
+    :param result:
+        Grid search results as a DataFrame.
+
+        Created by :py:func:`analyse_grid_search_result`.
+    """
+
