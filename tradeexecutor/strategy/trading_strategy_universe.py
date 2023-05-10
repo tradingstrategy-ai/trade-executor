e95a5b7433d1f0af2aa8d1563f7b4230bb7d1f94
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-05-10 14:56:30 +0200

    Add simpler trading universe creation

--- tradeexecutor/strategy/trading_strategy_universe.py
+++ tradeexecutor/strategy/trading_strategy_universe.py
@@ -255,23 +255,33 @@ class TradingStrategyUniverse(StrategyExecutionUniverse):
         """
 
         if pair:
-            chain_id, exchange_slug, base_token, quote_token, *fees = pair
+            # Create trading pair database
+            pair_universe = PandasPairUniverse.create_pair_universe(
+                dataset.pairs,
+                pairs=[pair],
+            )
 
-            if len(fees) > 0:
-                raise NotImplementedError("This method still lacks fee filtering")
+            chain_id = pair[0]
+            exchange_slug = pair[1]
 
-        # We only trade on Pancakeswap v2
-        exchange_universe = dataset.exchanges
-        exchange = exchange_universe.get_by_chain_and_slug(chain_id, exchange_slug)
-        assert exchange, f"No exchange {exchange_slug} found on chain {chain_id.name}"
+            # We only trade on Pancakeswap v2
+            exchange_universe = dataset.exchanges
+            exchange = exchange_universe.get_by_chain_and_slug(chain_id, exchange_slug)
+            assert exchange, f"No exchange {exchange_slug} found on chain {chain_id.name}"
 
-        # Create trading pair database
-        pair_universe = PandasPairUniverse.create_single_pair_universe(
-            dataset.pairs,
-            exchange,
-            base_token,
-            quote_token,
-        )
+        else:
+            # We only trade on Pancakeswap v2
+            exchange_universe = dataset.exchanges
+            exchange = exchange_universe.get_by_chain_and_slug(chain_id, exchange_slug)
+            assert exchange, f"No exchange {exchange_slug} found on chain {chain_id.name}"
+
+            # Create trading pair database
+            pair_universe = PandasPairUniverse.create_single_pair_universe(
+                dataset.pairs,
+                exchange,
+                base_token,
+                quote_token,
+            )
 
         # Get daily candles as Pandas DataFrame
         if dataset.candles is not None:
