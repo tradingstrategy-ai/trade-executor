66a5e572bbd8c2c1d6d7c39c8439a842ede4268f
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-05-15 23:54:01 +0200

    update crossover

--- tradeexecutor/state/crossover.py
+++ tradeexecutor/state/crossover.py
@@ -5,81 +5,218 @@
 """
 
 import pandas as pd
+import numpy as np
 
-def is_crossover_constant(series: pd.Series, value: float) -> bool:
-    """Detect if a series has crossed over a constant value. To be used in decide_trades() 
-    
-    :param series:
-        A pandas.Series object.
-        
-    :param value:
-        A constant value to compare against.
-        
-    :returns:
-        bool. True if the series has crossed over the value in the latest iteration, False otherwise.
-        
-    E.g. 
-    
-    decide_trades(...)
-        ...
-        if is_crossover_constant(fast_ema_series, 3300):
-            visualisation.plot_indicator(timestamp, "Crossover 2", PlotKind.technical_indicator_overlay_on_detached, fast_ema_latest, colour="black", detached_overlay_name="Fast EMA", plot_shape=PlotShape.marker)
-        ...
-    """
-    assert type(series) == pd.Series, "Series must be pandas.Series"
-    assert type(value) in {int, float}, "Value must be int or float"
-    
-    latest = series.iloc[-1]
-    
-    if latest == value:
-        return True
-    elif len(series) == 1:
-        return False
-    
-    previous = series.iloc[-2]
-    
-    return (
-        latest == value or
-        latest > value and previous < value
-        or latest < value and previous > value
-    )
-    
-def is_crossover(series1: pd.Series, series2: pd.Series) -> bool:
-    """Detect if two series have cross over. To be used in decide_trades() 
+epsilon = 1e-10  # This is just an example. You may need to adjust this value.
+
+
+def has_crossover_occurred(
+        series1: pd.Series, 
+        series2_or_int: pd.Series | int, 
+        min_values_above_cross: int | None = 1,
+        min_values_below_cross: int | None = 1,
+        min_percent_diff1: float | None = 0,
+        min_percent_diff2: float | None = 0,
+        min_gradient: float | None = None,
+    ) -> bool:
+    """Detect if the first series has crossed above the second series/int. Typically usage will be in decide_trades() 
     
     :param series1:
         A pandas.Series object.
         
-    :param series2:
-        A pandas.Series object.
+    :param series2_or_int:
+        A pandas.Series object or an int.
+    
+    :param min_values_above_cross:
+        The number of latest values of series1 that must have crossed above series2_or_int.
+
+    :param min_values_below_cross:
+        The minimum number of latest values before the crossover that must be below the crossover.
         
     :returns:
         bool. True if the series has crossed over the other series in the latest iteration, False otherwise.
         
-    E.g. 
-    
-    decide_trades(...)
-        ...
-        if is_crossover(fast_ema_series, slow_ema_series):
-            visualisation.plot_indicator(timestamp, "Crossover 1", PlotKind.technical_indicator_overlay_on_detached, fast_ema_latest, colour="blue", detached_overlay_name="Fast EMA", plot_shape=PlotShape.marker)
-        ...
     """
-    assert type(series1) == type(series2) == pd.Series, "Series must be pandas.Series"
+
+    series1 = _convert_list_to_series(series1)
+    series2_or_int = _convert_list_to_series(series2_or_int)
+
+    _validate_args(
+        series1, 
+        series2_or_int, 
+        min_values_above_cross, 
+        min_values_below_cross,
+        min_percent_diff1,
+        min_percent_diff2,
+    )
+
+
+    # Check for min_percent_difference
+    if min_percent_diff1:
+
+        if isinstance(series2_or_int, int):
+            series2 = [series2_or_int] * len(series1)
+        else:
+            series2 = series2_or_int
+
+        if not any(va for val1, val2 in zip(series1, series2)):
+            return False
     
-    s1_latest = series1.iloc[-1]
-    s2_latest = series2.iloc[-1]
-    
-    if s1_latest == s2_latest:
-        return True
-    elif len(series1) == 1 or len(series2) == 1:
+    if min_percent_diff1:
+
+        if series1.iloc[-1] == 0:
+            _epsilon = epsilon
+        else:
+            _epsilon = 0
+        
+        cross_magnitude1 = (series1.iloc[-1] - series1.iloc[0]) / (series1.iloc[0] + epsilon)
+
+        if abs(cross_magnitude1) < min_percent_diff1:
+            return False
+
+
+    if min_percent_diff2:
+
+        if isinstance(series2_or_int, int):
+            raise ValueError("min_percent_diff2 cannot be used when series2_or_int is an int")
+
+
+
+        if series2_or_int.iloc[-1] == 0:
+            _epsilon = epsilon
+        else:
+            _epsilon = 0
+
+        cross_magnitude2 = (series2_or_int.iloc[-1] - series2_or_int.iloc[0]) / (series2_or_int.iloc[0] + epsilon)
+
+        if abs(cross_magnitude2) < min_percent_diff2:
+            return False
+
+
+    during_lookback1 = series1.iloc[-min_values_above_cross:]
+
+    if isinstance(series2_or_int, int):
+        # values in the lookback period
+        during_lookback2 = [series2_or_int] * len(during_lookback1)
+
+        cross_value = series2_or_int
+    else:
+        # values in the lookback period
+        during_lookback2 = series2_or_int.iloc[-min_values_above_cross:]
+
+        # average of latest value before the lookback period and first value in the lookback period
+        cross_value = (series2_or_int.iloc[-min_values_above_cross-1] + series2_or_int.iloc[-min_values_above_cross]) / 2
+
+    # values before the lookback period
+    before_lookback1 = series1[:-min_values_above_cross]
+
+    # Validation for min_values_below_cross
+    if min_values_below_cross > len(before_lookback1):
+        return False
+
+    satisfied = [x < cross_value for x in before_lookback1[-min_values_below_cross:]]
+
+    # make sure not fake crossover
+    # at least `min_values_below_cross` values before the lookback period must be smaller than the cross_value
+    # the point right before crossover must obviously be smaller than the cross_value
+    if (
+        sum(satisfied) < min_values_below_cross
+        or before_lookback1.iloc[-1] > cross_value #  the point of crossover (can be equal to)
+    ): 
         return False
     
-    s1_prev = series1.iloc[-2]
-    s2_prev = series2.iloc[-2]
+    # Validation for min_gradient
+    # calculate gradient
+    if min_gradient is not None:
+        num_values = min_values_below_cross + min_values_above_cross
+        x = np.arange(num_values)
+        y = _standardize(series1.iloc[-num_values:].values)
+        slope, _ = np.polyfit(x, y, 1)
+        if slope < min_gradient:
+            return False
+    
+    
 
-    return (
-        (s1_latest > s2_latest
-        and s1_prev < s2_prev)
-        or (s1_latest < s2_latest
-        and s1_prev > s2_prev)
-    )
+    return all(val1 > val2 for val1, val2 in zip(during_lookback1, during_lookback2))
+
+
+def has_crossunder_occurred(series1: pd.Series, series2_or_int: pd.Series | int, min_values_above_cross: int | None = 1) -> bool:
+    """Detect if the first series has crossed above the second series/int. Typically usage will be in decide_trades() 
+    
+    :param series1:
+        A pandas.Series object.
+        
+    :param series2_or_int:
+        A pandas.Series object or an int.
+    
+    :param min_values_above_cross:
+        The number of latest values of series1 that must have crossed above series2_or_int.
+        
+    :returns:
+        bool. True if the series has crossed over the other series in the latest iteration, False otherwise.
+        
+    """
+
+    series1 = _convert_list_to_series(series1)
+    series2_or_int = _convert_list_to_series(series2_or_int)
+
+    _validate_args(series1, series2_or_int, min_values_above_cross)
+
+    during_lookback1 = series1.iloc[-min_values_above_cross:]
+
+    if isinstance(series2_or_int, int):
+        # values in the lookback period
+        during_lookback2 = [series2_or_int] * len(during_lookback1)
+        
+        # latest value before the lookback period
+        cross_value = series2_or_int
+    else:
+        # values in the lookback period
+        during_lookback2 = series2_or_int.iloc[-min_values_above_cross:]
+
+        # average of latest value before the lookback period and first value in the lookback period
+        cross_value = (series2_or_int.iloc[-min_values_above_cross-1] + series2_or_int.iloc[-min_values_above_cross]) / 2
+
+    # values before the lookback period
+    before_lookback1 = series1[:-min_values_above_cross]
+
+    # make sure not fake crossover
+    # at least 1 value before the lookback period must be greater than the cross_value
+    if not (any(before_lookback1 > cross_value) and before_lookback1.iloc[-1] >= cross_value): 
+        return False
+
+    return all(val1 < val2 for val1, val2 in zip(during_lookback1, during_lookback2))
+
+
+def _convert_list_to_series(series1):
+    if isinstance(series1, list):
+        series1 = pd.Series(series1)
+    return series1
+
+
+def _validate_args(
+    series1,
+    series2_or_int, 
+    min_values_above_cross, 
+    min_values_below_cross,
+    min_percent_diff1,
+    min_percent_diff2,
+):
+    assert type(series1) == pd.Series, "series1 must be a pandas.Series object"
+    assert type(series2_or_int) in [
+        pd.Series,
+        int,
+    ], "series2_or_int must be a pandas.Series object or an int"
+    assert type(min_values_above_cross) == int and min_values_above_cross > 0, "min_values_above_cross must be a positive int"
+    assert type(min_values_below_cross) == int and min_values_below_cross > 0, "min_values_below_cross must be a positive int"
+
+    assert type(min_percent_diff1) in {float, int} and 0 <= min_percent_diff1 <= 1, "min_percent_above must be a number between 0 and 1"
+
+    assert type(min_percent_diff2) in {float, int} and 0 <= min_percent_diff2 <= 1, "min_percent_below must be a number between 0 and 1
+
+
+def _standardize(series):
+    mean = series.mean()
+    std_dev = series.std()
+    standardized_series = (series - mean) / std_dev
+    return standardized_series
