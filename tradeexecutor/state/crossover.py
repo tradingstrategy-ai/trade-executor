1f5fca0792191d9b7f93828b4848aac2e7fa5644
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-05-16 04:38:29 +0200

    adjust crossover function

--- tradeexecutor/state/crossover.py
+++ tradeexecutor/state/crossover.py
@@ -19,6 +19,7 @@ def has_crossover_occurred(
         buffer_above_percent: float | None = 0,
         buffer_below_percent: float | None = 0,
         min_gradient: float | None = None,
+        return_cross_index: bool | None = False,
     ) -> bool:
     """Detect if the first series has crossed above the second series/int. Typically usage will be in decide_trades() 
     
@@ -39,6 +40,9 @@ def has_crossover_occurred(
     
     :param buffer_below_percent:
         The minimum percentage difference the series must be below the crossover value by for the crossover to be considered.
+    
+    :param return_cross_index:
+        If True, return the index of the crossover. If False, return True if a crossover has occurred, False otherwise.
         
     :returns:
         bool. True if the series has crossed over the other series in the latest iteration taking values_above_cross and values_below_cross into considertaion, False otherwise.
@@ -48,9 +52,12 @@ def has_crossover_occurred(
     series1 = _convert_list_to_series(series1)
     series2 = _convert_list_to_series(series2)
 
+    # make the series have the same length
     # trim series2 to the same length as series1 (using latest values)
     if len(series2) > len(series1):
         series2 = series2.iloc[-len(series1):]
+    elif len(series1) > len(series2):
+        series1 = series1.iloc[-len(series2):]
 
     _validate_args(
         series1, 
@@ -66,7 +73,7 @@ def has_crossover_occurred(
     for i,x in enumerate(series1):
         if i == 0:
             continue
-
+        
         if (series1.iloc[i-1] <= series2.iloc[i-1] and x > series2.iloc[i]):
             # cross_index will be first index after the crossover
             cross_index = i
@@ -79,38 +86,39 @@ def has_crossover_occurred(
                 cross_value = epsilon
 
             # don't break here, we want the latest index of crossover
+        
 
     if not cross_index:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     after_cross1 = series1.iloc[cross_index:]
     
     # buffer_above_percent
     if not (max(after_cross1) - cross_value)/cross_value >= buffer_above_percent:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     # min_values_above_cross
     if len(after_cross1) < min_values_above_cross:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
     after_cross1_cut = after_cross1.iloc[:min_values_above_cross]
     if any(x <= cross_value for x in after_cross1_cut):
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     before_cross1 = series1.iloc[:cross_index]
     # make sure there are values below the cross
     if not any(x < cross_value for x in before_cross1):
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     # min_values_below_cross
     if len(before_cross1) < min_values_below_cross:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
     before_cross1_cut = before_cross1.iloc[-min_values_below_cross:]
     if any(x > cross_value for x in before_cross1_cut):
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
     
     # buffer_below_percent
     if not (cross_value - min(before_cross1))/cross_value >= buffer_below_percent:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     # min_gradient
     if min_gradient is not None:
@@ -118,11 +126,11 @@ def has_crossover_occurred(
         y = _standardize(series1.values)
         slope, _ = np.polyfit(x, y, 1)
         if slope < min_gradient:
-            return False
+            return _get_return_value(False, return_cross_index, cross_index)
 
     # If we get here, then all conditions are met
 
-    return True
+    return _get_return_value(True, return_cross_index, cross_index)
 
 
 def has_crossunder_occurred(
@@ -133,6 +141,7 @@ def has_crossunder_occurred(
         buffer_above_percent: float | None = 0,
         buffer_below_percent: float | None = 0,
         min_gradient: float | None = None,
+        return_cross_index: bool | None = False,
     ) -> bool:
     """Detect if the first series has crossed below the second series/int. 
     
@@ -153,6 +162,9 @@ def has_crossunder_occurred(
     
     :param buffer_below_percent:
         The minimum percentage that the series must fall below the crossover value by for the crossover to be considered.
+    
+    :param return_cross_index:
+        If True, return the index of the crossunder. If False, return True if a crossunder has occurred, False otherwise.
         
     :returns:
         bool. True if the series has crossed under the other series in the latest iteration taking values_above_cross and values_below_cross into consideration, False otherwise.
@@ -161,9 +173,12 @@ def has_crossunder_occurred(
     series1 = _convert_list_to_series(series1)
     series2 = _convert_list_to_series(series2)
 
+    # make the series have the same length
     # trim series2 to the same length as series1 (using latest values)
     if len(series2) > len(series1):
         series2 = series2.iloc[-len(series1):]
+    elif len(series1) > len(series2):
+        series1 = series1.iloc[-len(series2):]
 
     _validate_args(
         series1, 
@@ -191,30 +206,30 @@ def has_crossunder_occurred(
                 cross_value = epsilon
 
     if not cross_index:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     after_cross1 = series1.iloc[cross_index:]
     if len(after_cross1) < min_values_below_cross:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
     if not (cross_value - min(after_cross1))/cross_value >= buffer_below_percent:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     after_cross1_cut = after_cross1.iloc[:min_values_below_cross]
     if any(x > cross_value for x in after_cross1_cut):
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     before_cross1 = series1.iloc[:cross_index]
     # make sure there are values above the cross
     if not any(x > cross_value for x in before_cross1):
         return False
     if len(series1.iloc[:-cross_index]) < min_values_above_cross:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
     if not (max(before_cross1) - cross_value)/cross_value >= buffer_above_percent:
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     before_cross1_cut = before_cross1.iloc[-min_values_above_cross:]
     if any(x < cross_value for x in before_cross1_cut):
-        return False
+        return _get_return_value(False, return_cross_index, cross_index)
 
     # min_gradient
     if min_gradient is not None:
@@ -222,11 +237,23 @@ def has_crossunder_occurred(
         y = _standardize(series1.values)
         slope, _ = np.polyfit(x, y, 1)
         if slope > -min_gradient:
-            return False
+            return _get_return_value(False, return_cross_index, cross_index)
 
     # If we get here, then all conditions are met
 
-    return True
+    return _get_return_value(True, return_cross_index, cross_index)
+
+
+def _get_return_value(
+        return_value: bool,
+        return_cross_index: bool,
+        cross_index: int | None,
+    ) -> bool:
+    """Return the correct return value based on the value of return_cross_index."""
+    if return_cross_index:
+        return return_value, cross_index
+    else:
+        return return_value
 
 
 def _convert_list_to_series(series):
