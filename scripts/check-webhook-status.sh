35306bc583649a60af07d3a995db94de0036e934
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-06-26 11:06:13 +0200

    Update the check webhook script

--- scripts/check-webhook-status.sh
+++ scripts/check-webhook-status.sh
@@ -23,8 +23,13 @@ set -u
 webhook_url=$1
 
 # /status gives 200 in the case the trade-executor has crashed
-# and you need to check for the exception record in the status output
-failure_reason=$(curl --silent --fail "$webhook_url/status" | jq ".exception")
+# and you need to check for the exception record in the status output.
+#
+# Have some built-in curl retry logic to reading the webhook,
+# instead of relying on Docker healthcheck retries.
+# https://stackoverflow.com/a/64041910/315168
+#
+failure_reason=$(curl --silent --fail --retry 6 --max-time 5 --retry-delay 10 --retry-max-time 60 "$webhook_url/status" | jq ".exception")
 
 if [ "$failure_reason" != "null" ] ; then
     echo "trade-executor has crashed: $failure_reason"
