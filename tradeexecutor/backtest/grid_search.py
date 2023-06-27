b88477286d962edf3e2f04addf7069a8621c59dc
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-06-27 11:06:41 +0200

    Make sure we correctly handle decision cycle frequency when calculating statistics for grid search

--- tradeexecutor/backtest/grid_search.py
+++ tradeexecutor/backtest/grid_search.py
@@ -45,6 +45,7 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class GridParameter:
+    """One value in grid search matrix."""
     name: str
     value: Any
 
@@ -293,29 +294,8 @@ def run_grid_combination_multiprocess(
 
     return result
 
-def hide_warnings(func):
-    """wrap func to suppress warnings"""
 
-    # Hidden warnings include:
-    
-    # In perform_grid_search:
-    # /home/.cache/pypoetry/virtualenvs/trade-executor-xSh0vQvh-py3.10/lib/python3.10/site-packages/numpy/lib/function_base.py:2854: 
-    # RuntimeWarning: invalid value encountered in divide
-    # c /= stddev[:, None]
-
-    # In perform_grid_search:
-    # /home/alex/.cache/pypoetry/virtualenvs/trade-executor-xSh0vQvh-py3.10/lib/python3.10/site-packages/scipy/stats/_distn_infrastructure.py:2351: 
-    # RuntimeWarning: invalid value encountered in multiply
-    # lower_bound = _a * scale + loc
-
-    def wrapper(*args, **kwargs):
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            return func(*args, **kwargs)
-
-    return wrapper
-
-@hide_warnings
+@_hide_warnings
 def perform_grid_search(
         grid_search_worker: GridSearchWorker,
         universe: TradingStrategyUniverse,
@@ -616,6 +596,7 @@ _process_pool: concurrent.futures.process.ProcessPoolExecutor | None = None
 
 def _process_init(pickled_universe):
     """Child worker process initialiser."""
+    # Transfer ove the universe to the child process
     global _universe
     _universe = pickle.loads(pickled_universe)
 
@@ -627,3 +608,26 @@ def _handle_sigterm(*args):
     for p in processes:
         p.kill()
     sys.exit(1)
+
+
+def _hide_warnings(func):
+    """wrap func to suppress warnings"""
+
+    # Hidden warnings include:
+
+    # In perform_grid_search:
+    # /home/.cache/pypoetry/virtualenvs/trade-executor-xSh0vQvh-py3.10/lib/python3.10/site-packages/numpy/lib/function_base.py:2854:
+    # RuntimeWarning: invalid value encountered in divide
+    # c /= stddev[:, None]
+
+    # In perform_grid_search:
+    # /home/alex/.cache/pypoetry/virtualenvs/trade-executor-xSh0vQvh-py3.10/lib/python3.10/site-packages/scipy/stats/_distn_infrastructure.py:2351:
+    # RuntimeWarning: invalid value encountered in multiply
+    # lower_bound = _a * scale + loc
+
+    def wrapper(*args, **kwargs):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return func(*args, **kwargs)
+
+    return wrapper
