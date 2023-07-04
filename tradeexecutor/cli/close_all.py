43c7fdd5db65c0bcee762a63f1d0ffb19925a630
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-07-04 11:50:23 +0200

    Working on a command to close all positions

---
+++ tradeexecutor/cli/close_all.py
@@ -0,0 +1,176 @@
+"""Closing all positions externally.
+
+Code to clean up positions or forcing a shutdown.
+"""
+import logging
+import datetime
+import textwrap
+from decimal import Decimal
+from typing import Union
+
+from web3 import Web3
+
+from tradeexecutor.analysis.position import display_positions
+from tradeexecutor.ethereum.enzyme.vault import EnzymeVaultSyncModel
+from tradeexecutor.strategy.sync_model import SyncModel
+from tradingstrategy.universe import Universe
+from tradingstrategy.pair import HumanReadableTradingPairDescription
+
+from tradeexecutor.ethereum.hot_wallet_sync_model import EthereumHotWalletReserveSyncer
+from tradeexecutor.state.state import State
+from tradeexecutor.strategy.execution_model import ExecutionModel
+from tradeexecutor.strategy.pandas_trader.position_manager import PositionManager
+from tradeexecutor.strategy.pricing_model import PricingModel
+from tradeexecutor.strategy.routing import RoutingModel, RoutingState
+from tradeexecutor.strategy.trading_strategy_universe import TradingStrategyUniverse, translate_trading_pair
+
+logger = logging.getLogger(__name__)
+
+
+def close_all(
+        web3: Web3,
+        execution_model: ExecutionModel,
+        pricing_model: PricingModel,
+        sync_model: SyncModel,
+        state: State,
+        universe: TradingStrategyUniverse,
+        routing_model: RoutingModel,
+        routing_state: RoutingState,
+        amount=Decimal("1.0"),
+        pair: HumanReadableTradingPairDescription | None = None,
+):
+    """Perform a test trade.
+
+    Buy and sell 1 token worth for 1 USD to check that
+    our trade routing works.
+    """
+
+    assert isinstance(sync_model, SyncModel)
+
+    ts = datetime.datetime.utcnow()
+
+    # Sync nonce for the hot wallet
+    execution_model.initialize()
+
+    data_universe: Universe = universe.universe
+
+    reserve_asset = universe.get_reserve_asset()
+
+    if pair:
+        raw_pair = data_universe.pairs.get_pair(*pair)
+    else:
+        raw_pair = data_universe.pairs.get_single()
+
+    pair = translate_trading_pair(raw_pair)
+
+    # Get estimated price for the asset we are going to buy
+    assumed_price_structure = pricing_model.get_buy_price(
+        ts,
+        pair,
+        amount,
+    )
+
+    logger.info("Making a test trade on pair: %s, for %f %s price is %f %s/%s",
+                pair,
+                amount,
+                reserve_asset.token_symbol,
+                assumed_price_structure.mid_price,
+                pair.base.token_symbol,
+                reserve_asset.token_symbol,
+                )
+
+    logger.info("Sync model is %s", sync_model)
+    logger.info("Trading university reserve asset is %s", universe.get_reserve_asset())
+
+    # Sync any incoming stablecoin transfers
+    # that have not been synced yet
+    balance_updates = sync_model.sync_treasury(
+        ts,
+        state,
+        list(universe.reserve_assets),
+    )
+
+    logger.info("We received balance update events: %s", balance_updates)
+
+    vault_address = sync_model.get_vault_address()
+    hot_wallet = sync_model.get_hot_wallet()
+    gas_at_start = hot_wallet.get_native_currency_balance(web3)
+
+    logger.info("Account data before starting to close all")
+    logger.info("  Vault address: %s", vault_address)
+    logger.info("  Hot wallet address: %s", hot_wallet.address)
+    logger.info("  Hot wallet balance: %s", gas_at_start)
+
+    # TODO: Move to SyncModel
+    if isinstance(sync_model, EnzymeVaultSyncModel):
+        # Check if the vault was properly set up
+        vault = sync_model.vault
+        assert vault.vault.functions.isAssetManager(hot_wallet.address).call(), f"Address is not set up as Enzyme asset manager: {hot_wallet.address}"
+
+        logger.info("  Vault owner: %s", vault.vault.functions.getOwner().call())
+
+    if len(state.portfolio.reserves) == 0:
+        raise RuntimeError("No reserves detected for the strategy. Does your wallet/vault have USDC deposited for trading?")
+
+    reserve_currency = state.portfolio.get_default_reserve_position().asset.token_symbol
+    reserve_currency_at_start = state.portfolio.get_default_reserve_position().get_value()
+
+    logger.info("  Reserve currency balance: %s %s", reserve_currency_at_start, reserve_currency)
+
+    assert reserve_currency_at_start > 0, f"No deposits available to trade. Vault at {vault_address}"
+
+    # Create PositionManager helper class
+    # that helps open and close positions
+    position_manager = PositionManager(
+        ts,
+        universe.universe,
+        state,
+        pricing_model,
+    )
+
+    # The message left on the test positions and trades
+    notes = f"Closed with close-all command at {datetime.datetime.utcnow()}"
+
+    # Open the test position only if there isn't position already open
+    # on the previous run
+
+    open_positions = list(state.portfolio.open_positions.values())
+    logger.info("Currently having %d open positions", len(open_positions))
+
+    for p in open_positions:
+        # Create trades to open the position
+        logger.info("Closing position %s", p)
+        trades = position_manager.close_position(p)
+
+        assert len(trades) == 1
+        trade = trades[0]
+
+        # Compose the trades as approve() + swapTokenExact(),
+        # broadcast them to the blockchain network and
+        # wait for the confirmation
+        execution_model.execute_trades(
+            ts,
+            state,
+            trades,
+            routing_model,
+            routing_state,
+        )
+
+        if not trade.is_success():
+            logger.error("Trade failed: %s", trade)
+            logger.error("Tx hash: %s", trade.blockchain_transactions[-1].tx_hash)
+            logger.error("Revert reason: %s", trade.blockchain_transactions[-1].revert_reason)
+            logger.error("Trade dump:\n%s", trade.get_full_debug_dump_str())
+            raise AssertionError("Trade to close position failed")
+
+    gas_at_end = hot_wallet.get_native_currency_balance(web3)
+    reserve_currency_at_end = state.portfolio.get_default_reserve_position().get_value()
+
+    logger.info("Trade report")
+    logger.info("  Gas spent: %s", gas_at_start - gas_at_end)
+    logger.info("  Trades done currently: %d", len(list(state.portfolio.get_all_trades())))
+    logger.info("  Reserves currently: %s %s", reserve_currency_at_end, reserve_currency)
+    logger.info("  Reserve currency spent: %s %s", reserve_currency_at_start - reserve_currency_at_end, reserve_currency)
+
+    position_info = textwrap.indent(str(display_positions(open_positions)), prefix="  ")
+    logger.info("Position data for positions that were closed:\n%s", position_info)
---
+++ tradeexecutor/cli/commands/close_all.py
@@ -0,0 +1,217 @@
+"""close-all command"""
+
+import datetime
+import logging
+from pathlib import Path
+from typing import Optional
+import re
+import typer
+
+from .app import app
+from ..bootstrap import prepare_executor_id, prepare_cache, create_web3_config, create_state_store, \
+    create_execution_and_sync_model, create_client
+from ..log import setup_logging
+from ..testtrade import make_test_trade
+from ...strategy.approval import UncheckedApprovalModel
+from ...strategy.bootstrap import make_factory_from_strategy_mod
+from ...strategy.description import StrategyExecutionDescription
+from ...strategy.execution_context import ExecutionContext, ExecutionMode
+from ...strategy.execution_model import AssetManagementMode
+from ...strategy.run_state import RunState
+from ...strategy.strategy_module import read_strategy_module, StrategyModuleInformation
+from ...strategy.trading_strategy_universe import TradingStrategyUniverseModel
+from ...strategy.universe_model import UniverseOptions
+from ...utils.timer import timed_task
+from tradeexecutor.cli.commands import shared_options
+from tradingstrategy.chain import ChainId
+
+
+@app.command()
+def close_all(
+    id: str = shared_options.id,
+    name: Optional[str] = shared_options.name,
+
+    strategy_file: Path = shared_options.strategy_file,
+    trading_strategy_api_key: str = shared_options.trading_strategy_api_key,
+    state_file: Optional[Path] = shared_options.state_file,
+    cache_path: Optional[Path] = shared_options.cache_path,
+
+    log_level: str = shared_options.log_level,
+    json_rpc_binance: Optional[str] = shared_options.json_rpc_binance,
+    json_rpc_polygon: Optional[str] = shared_options.json_rpc_polygon,
+    json_rpc_avalanche: Optional[str] = shared_options.json_rpc_avalanche,
+    json_rpc_ethereum: Optional[str] = shared_options.json_rpc_ethereum,
+    json_rpc_arbitrum: Optional[str] = shared_options.json_rpc_arbitrum,
+    json_rpc_anvil: Optional[str] = shared_options.json_rpc_anvil,
+    private_key: str = shared_options.private_key,
+
+    asset_management_mode: AssetManagementMode = shared_options.asset_management_mode,
+    vault_address: Optional[str] = shared_options.vault_address,
+    vault_adapter_address: Optional[str] = shared_options.vault_adapter_address,
+    vault_payment_forwarder_address: Optional[str] = shared_options.vault_payment_forwarder,
+    min_gas_balance: Optional[float] = shared_options.min_gas_balance,
+    max_slippage: float = shared_options.max_slippage,
+
+    confirmation_block_count: int = shared_options.confirmation_block_count,
+
+    # Test EVM backend when running e2e tests
+    test_evm_uniswap_v2_router: Optional[str] = shared_options.test_evm_uniswap_v2_router,
+    test_evm_uniswap_v2_factory: Optional[str] = shared_options.test_evm_uniswap_v2_factory,
+    test_evm_uniswap_v2_init_code_hash: Optional[str] = shared_options.test_evm_uniswap_v2_init_code_hash,
+
+    # for multipair strategies
+    pair: Optional[str] = shared_options.pair,
+):
+    """Close all open positions.
+
+    - Syncs the latest reserve deposits and redemptions
+
+    - Closes any position that's open currently
+    """
+
+    if pair:
+        pair = parse_pair_data(pair)
+
+    id = prepare_executor_id(id, strategy_file)
+
+    logger = setup_logging(log_level=log_level)
+
+    mod: StrategyModuleInformation = read_strategy_module(strategy_file)
+
+    cache_path = prepare_cache(id, cache_path)
+
+    execution_context = ExecutionContext(
+        mode=ExecutionMode.preflight_check,
+        timed_task_context_manager=timed_task
+    )
+
+    web3config = create_web3_config(
+        json_rpc_binance=json_rpc_binance,
+        json_rpc_polygon=json_rpc_polygon,
+        json_rpc_avalanche=json_rpc_avalanche,
+        json_rpc_ethereum=json_rpc_ethereum,
+        json_rpc_anvil=json_rpc_anvil,
+        json_rpc_arbitrum=json_rpc_arbitrum,
+    )
+
+    if not web3config.has_any_connection():
+        raise RuntimeError("Vault deploy requires that you pass JSON-RPC connection to one of the networks")
+
+    web3config.choose_single_chain()
+
+    execution_model, sync_model, valuation_model_factory, pricing_model_factory = create_execution_and_sync_model(
+        asset_management_mode=asset_management_mode,
+        private_key=private_key,
+        web3config=web3config,
+        confirmation_timeout=datetime.timedelta(seconds=60),
+        confirmation_block_count=confirmation_block_count,
+        min_gas_balance=min_gas_balance,
+        max_slippage=max_slippage,
+        vault_address=vault_address,
+        vault_adapter_address=vault_adapter_address,
+        vault_payment_forwarder_address=vault_payment_forwarder_address,
+        routing_hint=mod.trade_routing,
+    )
+
+    client, routing_model = create_client(
+        mod=mod,
+        web3config=web3config,
+        trading_strategy_api_key=trading_strategy_api_key,
+        cache_path=cache_path,
+        test_evm_uniswap_v2_factory=test_evm_uniswap_v2_factory,
+        test_evm_uniswap_v2_router=test_evm_uniswap_v2_router,
+        test_evm_uniswap_v2_init_code_hash=test_evm_uniswap_v2_init_code_hash,
+        clear_caches=False,
+    )
+    assert client is not None, "You need to give details for TradingStrategy.ai client"
+
+    if not state_file:
+        state_file = f"state/{id}.json"
+
+    store = create_state_store(Path(state_file))
+
+    if store.is_pristine():
+        assert name, "Strategy state file has not been createad. You must pass strategy name to create."
+        state = store.create(name)
+    else:
+        state = store.load()
+
+    # Set up the strategy engine
+    factory = make_factory_from_strategy_mod(mod)
+    run_description: StrategyExecutionDescription = factory(
+        execution_model=execution_model,
+        execution_context=execution_context,
+        timed_task_context_manager=execution_context.timed_task_context_manager,
+        sync_model=sync_model,
+        valuation_model_factory=valuation_model_factory,
+        pricing_model_factory=pricing_model_factory,
+        approval_model=UncheckedApprovalModel(),
+        client=client,
+        routing_model=routing_model,
+        run_state=RunState(),
+    )
+
+    # We construct the trading universe to know what's our reserve asset
+    universe_model: TradingStrategyUniverseModel = run_description.universe_model
+    ts = datetime.datetime.utcnow()
+    universe = universe_model.construct_universe(
+        ts,
+        ExecutionMode.preflight_check,
+        UniverseOptions())
+
+    runner = run_description.runner
+    routing_state, pricing_model, valuation_method = runner.setup_routing(universe)
+
+    make_test_trade(
+        web3config.get_default(),
+        execution_model,
+        pricing_model,
+        sync_model,
+        state,
+        universe,
+        runner.routing_model,
+        routing_state,
+        pair=pair,
+    )
+
+    # Store the test trade data in the strategy history
+    store.sync(state)
+
+    logger.info("All ok")
+
+
+def parse_pair_data(s: str):
+    """Extract pair data from string.
+    
+    :param s:
+        String in the format of: [(chain_id, exchange_slug, base_token, quote_token, fee)])], 
+        
+        where rate is optional.
+
+    :raises ValueError:
+        If the string is not in the correct format.
+    
+    :return:
+        Tuple of (chain_id, exchange_slug, base_token, quote_token, fee)"""
+    
+    try: 
+        # Extract the tuple
+        tuple_str = re.search(r'\((.*?)\)', s)[1]
+
+        # Split elements and remove leading/trailing whitespaces
+        elements = [e.strip() for e in tuple_str.split(',')]
+
+        if len(elements) not in {4, 5}:
+            raise ValueError()
+
+        # Process elements
+        chain_id = getattr(ChainId, elements[0].split('.')[-1])
+        exchange_slug = elements[1].strip('"')
+        base_token = elements[2].strip('"')
+        quote_token = elements[3].strip('"')
+        fee = float(elements[4]) if len(elements) > 4 else None
+
+    except:
+        raise ValueError(f'Invalid pair data: {s}. Tuple must be in the format of: (chain_id, exchange_slug, base_token, quote_token, fee), where fee is optional')
+
+    return (chain_id, exchange_slug, base_token, quote_token, fee)
