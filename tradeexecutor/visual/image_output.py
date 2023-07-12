20a8c033e834f2bf037532f336b546ba82d82e97
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-07-12 22:45:39 +0200

    add util function

--- tradeexecutor/visual/image_output.py
+++ tradeexecutor/visual/image_output.py
@@ -6,6 +6,8 @@
 from io import BytesIO
 
 import plotly.graph_objects as go
+import webbrowser
+from pathlib import Path
 
 
 def render_plotly_figure_as_image_file(
@@ -36,3 +38,19 @@ def render_plotly_figure_as_image_file(
     data = stream.getvalue()
     stream.close()
     return data
+
+
+def open_plotly_figure_in_browser(figure: go.Figure) -> None:
+    """Open Plotly figure in a browser. Useful for debugging.
+
+    See https://stackoverflow.com/a/74619515/315168
+    
+    :param figure:
+        Plotly figure
+    """
+    png_data = render_plotly_figure_as_image_file(figure)
+    path = Path("/tmp/test-image.png")
+    with open(path, "wb") as out:
+        out.write(png_data)
+
+    webbrowser.open(f"file://{path.as_posix()}")
