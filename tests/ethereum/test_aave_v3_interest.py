c483b4c9521beb1b643e4404bac4d3bca2b0d7f2
Hieu Nguyen <hieu@tradingstrategy.ai>
2023-08-01 23:56:06 +0300

    Add more tests

--- tests/ethereum/test_aave_v3_interest.py
+++ tests/ethereum/test_aave_v3_interest.py
@@ -42,34 +42,29 @@ def test_get_aave_v3_raw_data_for_period(persistent_test_client):
 @pytest.mark.parametrize(
     "token, chain_id, amount, start_time, end_time",
     (
-        # (
-        #     "USDC",
-        #     137,
-        #     10000 * 10**6,
-        #     datetime.datetime(2023, 1, 1),
-        #     datetime.datetime(2023, 1, 3),
-        # ),
-        # (
-        #     "USDT",
-        #     137,
-        #     10000 * 10**6,
-        #     datetime.datetime(2023, 2, 1),
-        #     datetime.datetime(2023, 2, 2),
-        # ),
-        # (
-        #     "USDT",
-        #     137,
-        #     10000 * 10**6,
-        #     datetime.datetime(2023, 3, 10),
-        #     datetime.datetime(2023, 3, 13),
-        # ),
+        (
+            "USDC",
+            137,
+            10000 * 10**6,
+            datetime.datetime(2023, 1, 1),
+            datetime.datetime(2023, 1, 3),
+        ),
         (
             "USDT",
-            1,
-            5000000000,
-            datetime.datetime(2023, 2, 13, 18, 51, 35),
-            datetime.datetime(2023, 2, 13, 18, 58, 11),
+            137,
+            10000 * 10**6,
+            datetime.datetime(2023, 2, 1),
+            datetime.datetime(2023, 2, 2),
         ),
+        (
+            "USDT",
+            137,
+            10000 * 10**6,
+            datetime.datetime(2023, 3, 10),
+            datetime.datetime(2023, 3, 13),
+        ),
+
+        # actual borrow on chain with repay data
         (
             "USDC",
             1,
@@ -84,13 +79,13 @@ def test_get_aave_v3_raw_data_for_period(persistent_test_client):
             datetime.datetime(2023, 2, 10, 3, 44, 23),
             datetime.datetime(2023, 2, 13, 16, 30, 23),
         ),
-        (
-            "USDT",
-            137,
-            10000 * 10**6,
-            datetime.datetime(2023, 3, 10),
-            datetime.datetime(2023, 3, 13),
-        ),
+        # (
+        #     "USDT",
+        #     1,
+        #     5000000000,
+        #     datetime.datetime(2023, 2, 13, 18, 51, 35),
+        #     datetime.datetime(2023, 2, 13, 18, 58, 11),
+        # ),
     ),
 )
 def test_estimate_loan_interests(persistent_test_client, token, chain_id, amount, start_time, end_time):
@@ -99,8 +94,13 @@ def test_estimate_loan_interests(persistent_test_client, token, chain_id, amount
     """
     client = persistent_test_client
 
+
+    decimals = 18
+    if token in ("USDC", "USDT"):
+        decimals = 6
+
     print()
-    print(f"Borrow {token} with amount {amount} from {start_time} to {end_time}")
+    print(f"Borrow {amount / 10**decimals} {token} from {start_time} to {end_time} ({(end_time - start_time).days} days)")
 
     calculated_interests = calculate_loan_interests_raw(
         client,
@@ -111,7 +111,7 @@ def test_estimate_loan_interests(persistent_test_client, token, chain_id, amount
         end_time,
     )
 
-    print(f"Calculated: {calculated_interests}")
+    print(f"Calculated interests: {calculated_interests / 10**decimals:.5f}")
     assert calculated_interests > 0
 
     estimated_interests = estimate_loan_interests_raw(
@@ -123,10 +123,11 @@ def test_estimate_loan_interests(persistent_test_client, token, chain_id, amount
         end_time,
     )
 
-    print(f"Estimated: {estimated_interests}")
+    print(f"Estimated interests using hourly Aave candles: {estimated_interests / 10**decimals:.5f}")
     assert estimated_interests > 0
 
     # assert estimated_interests > calculated_interests
 
-    print("Diff:", estimated_interests * 100 / calculated_interests - 100, "%")
+    diff = estimated_interests * 100 / calculated_interests - 100
+    print(f"Diff: {diff:.4f}%")
     print()
