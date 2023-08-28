5ac7868017af04c7e6552da0b71e7cd6c867bcb0
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-08-28 12:37:27 +0200

    change float or int to decimal

--- tradeexecutor/ethereum/uniswap_v2/uniswap_v2_live_pricing.py
+++ tradeexecutor/ethereum/uniswap_v2/uniswap_v2_live_pricing.py
@@ -90,7 +90,7 @@ class UniswapV2LivePricing(EthereumPricingModel):
         if quantity is None:
             quantity = Decimal(self.very_small_amount)
 
-        quantity = Decimal(quantity) if isinstance(quantity, float) else quantity
+        quantity = Decimal(quantity) if isinstance(quantity, float | int) else quantity
 
         assert isinstance(quantity, Decimal)
 
@@ -175,7 +175,7 @@ class UniswapV2LivePricing(EthereumPricingModel):
         if reserve is None:
             reserve = Decimal(self.very_small_amount)
         else:
-            reserve = Decimal(reserve) if isinstance(reserve, float) else reserve
+            reserve = Decimal(reserve) if isinstance(reserve, float | int) else reserve
             assert isinstance(reserve, Decimal), f"Reserve must be decimal, got {reserve.__class__}: {reserve}"
 
         target_pair, intermediate_pair = self.routing_model.route_pair(self.pair_universe, pair)
