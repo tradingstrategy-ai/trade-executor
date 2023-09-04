a6357b6d9c4987cc5e7c428bcb6115da01b29236
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-09-04 16:26:23 +0200

    create module for containing uniswap testing data

---
+++ tradeexecutor/testing/evm_uniswap_testing_data.py
@@ -0,0 +1,128 @@
+import json
+
+
+class UniswapV2TestData:
+    def __init__(self, version, factory, router, init_code_hash, exchange_slug):
+        assert (
+            type(factory)
+            == type(router)
+            == type(init_code_hash)
+            == type(exchange_slug)
+            == str
+        ), "UniswapV2TestData: All arguments must be strings."
+
+        assert version == "V2", "UniswapV2TestData: version must be V2"
+        self.version = "V2"
+        self.factory = factory
+        self.router = router
+        self.init_code_hash = init_code_hash
+        self.exchange_slug = exchange_slug
+
+
+class UniswapV3TestData:
+    def __init__(
+        self, version, factory, router, position_manager, quoter, exchange_slug
+    ):
+        assert (
+            type(factory)
+            == type(router)
+            == type(position_manager)
+            == type(quoter)
+            == str
+        ), "UniswapV3TestData: All arguments must be strings."
+
+        assert version == "V3", "UniswapV3TestData: version must be V3"
+        self.version = "V3"
+        self.factory = factory
+        self.router = router
+        self.position_manager = position_manager
+        self.quoter = quoter
+        self.exchange_slug = exchange_slug
+
+
+def serialize_uniswap_test_data(data: UniswapV2TestData | UniswapV3TestData) -> str:
+    """Serializes UniswapV2TestData or UniswapV3TestData to JSON string.
+
+    :param data: UniswapV2TestData or UniswapV3TestData
+
+    :returns: JSON string
+    """
+    return json.dumps({"version": data.version, "data": data.__dict__})
+
+
+def deserialize_uniswap_test_data(
+    json_str: str,
+) -> UniswapV2TestData | UniswapV3TestData:
+    """Deserializes JSON string to UniswapV2TestData or UniswapV3TestData.
+
+    :param json_str: JSON string
+
+    :returns: UniswapV2TestData or UniswapV3TestData
+    """
+    json_data = json.loads(json_str)
+    version = json_data.get("version")
+    data = json_data.get("data")
+
+    if version == "V2":
+        return UniswapV2TestData(**data)
+    elif version == "V3":
+        return UniswapV3TestData(**data)
+    else:
+        raise ValueError("Unknown version")
+
+
+def serialize_uniswap_test_data_list(
+    data_list: list[UniswapV2TestData | UniswapV3TestData],
+) -> str:
+    """Serializes list of UniswapV2TestData or UniswapV3TestData to JSON string.
+
+    :param data_list: List of UniswapV2TestData or UniswapV3TestData
+
+    :returns: JSON string
+    """
+    assert (
+        type(data_list) == list
+    ), "serialize_uniswap_test_data_list: data_list must be a list"
+    serialized_list = []
+    for data in data_list:
+        assert isinstance(data, UniswapV2TestData | UniswapV3TestData)
+        serialized_list.append(serialize_uniswap_test_data(data))
+    return json.dumps(serialized_list)
+
+
+def deserialize_uniswap_test_data_list(
+    json_str: str,
+) -> list[UniswapV2TestData | UniswapV3TestData]:
+    """Deserializes JSON string to list of UniswapV2TestData or UniswapV3TestData.
+
+    :param json_str: JSON string
+
+    :returns: List of UniswapV2TestData or UniswapV3TestData"""
+    serialized_list = json.loads(json_str)
+    unvalidated = [
+        deserialize_uniswap_test_data(serialized_item)
+        for serialized_item in serialized_list
+    ]
+    validated = validate_uniswap_test_data_list(unvalidated)
+    return validated
+
+
+def validate_uniswap_test_data_list(
+        test_data: list[UniswapV2TestData | UniswapV3TestData]
+):
+    """Validates list of UniswapV2TestData or UniswapV3TestData.
+    
+    :param test_data:
+        List of UniswapV2TestData or UniswapV3TestData
+
+    :raises AssertionError: If test_data is not a list of UniswapV2TestData or UniswapV3TestData
+
+    :returns: None
+    """
+    assert (
+        type(test_data) == list
+    ), "validate_uniswap_test_data_list: test_data must be a list"
+    for data in test_data:
+        assert isinstance(data, UniswapV2TestData | UniswapV3TestData)
+        assert data.version == "V2" or data.version == "V3"
+    return test_data
