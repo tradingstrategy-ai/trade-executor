b3a51f482343c05eb51c5934887f22d79c971513
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-09-05 14:00:56 +0200

    fix decide_trades and create_trading_universe

--- strategies/test_only/enzyme_end_to_end_multipair_generic_routing.py
+++ strategies/test_only/enzyme_end_to_end_multipair_generic_routing.py
@@ -21,6 +21,7 @@ from tradingstrategy.pair import DEXPair, HumanReadableTradingPairDescription
 from tradingstrategy.testing.uniswap_v2_mock_client import UniswapV2MockClient
 from tradingstrategy.timebucket import TimeBucket
 from tradingstrategy.universe import Universe
+from tradingstrategy.chain import ChainId
 
 from tradeexecutor.state.state import State
 from tradeexecutor.state.trade import TradeExecution
@@ -76,16 +77,17 @@ def decide_trades(
         # For odd seconds buy, for even seconds sell
         if cycle_number % 2 == 0:
             # buy on even days
-            if not position_manager.is_any_open():
+            if not position_manager.is_any_open_for_pair(pair):
                 position_size = 0.10
                 buy_amount = cash * position_size
                 trades += position_manager.open_1x_long(pair, buy_amount)
         else:
             # sell on odd days
-            if position_manager.is_any_open():
-                trades += position_manager.close_all()
+            if position_manager.is_any_open_for_pair(pair):
+                position = position_manager.get_current_position_for_pair(pair)
+                trades += position_manager.close_position(position)
 
-        return trades
+    return trades
 
 
 def create_trading_universe(
@@ -114,11 +116,16 @@ def create_trading_universe(
     reserve_asset_address = client.get_default_quote_token_address()
     reserve_asset_pair_ticker = None
     pairs: HumanReadableTradingPairDescription = []
+    
     for row in dataset.pairs.itertuples():
         assert row.chain_id == pair.chain_id, "All pairs must be on the same chain"
         # assert row.exchange_slug == pair.exchange_slug, "All pairs must be on the same exchange"
         
-        pairs.append([row.base_token_symbol, row.quote_token_symbol])
+        assert row.fee > 1, "fee should be in bps"
+        fee_multiplier = float(row.fee/10_000)
+        assert 0 < fee_multiplier < 1, "fee multiplier should be between 0 and 1"
+
+        pairs.append((ChainId(row.chain_id), row.exchange_slug, row.base_token_symbol, row.quote_token_symbol, float(row.fee/10_000)))
 
         # find reserve asset ticker
         if reserve_asset_address in {row.token0_address, row.token1_address}:
@@ -126,11 +133,12 @@ def create_trading_universe(
 
     assert reserve_asset_pair_ticker is not None, "Could not find reserve asset ticker"
 
-    universe = TradingStrategyUniverse.create_limited_pair_universe(
+    reserve_token_symbol = reserve_asset_pair_ticker[1]
+    assert reserve_token_symbol == "USDC", "We expect USDC to be the reserve asset. Got: {reserve_token_symbol}"
+
+    universe = TradingStrategyUniverse.create_multichain_universe_by_pair_descriptions(
         dataset,
-        pair.chain_id,
-        pair.exchange_slug,
         pairs,
-        reserve_asset_pair_ticker,
+        reserve_token_symbol,
     )
     return universe
