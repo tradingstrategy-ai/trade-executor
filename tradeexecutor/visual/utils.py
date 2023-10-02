910241545d76e7bfbbb70e3e43275362cf49128b
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-10-02 11:36:30 +0200

    replace format with f string

--- tradeexecutor/visual/utils.py
+++ tradeexecutor/visual/utils.py
@@ -114,7 +114,7 @@ def export_trade_for_dataframe(p: Portfolio, t: TradeExecution) -> dict:
                 ]
         
         if t.cost_of_gas:
-            label += ["Gas fee: {:.4f}".format(t.cost_of_gas)]
+            label += [f"Gas fee: {t.cost_of_gas:.4f}"]
 
     # See Plotly Scatter usage https://stackoverflow.com/a/61349739/315168
     return {
