9883379232bea3f160369de5ad2939fbb474adfa
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-10-24 21:25:29 +0200

    Report tx hashes on unfinished trades

--- tradeexecutor/state/state.py
+++ tradeexecutor/state/state.py
@@ -781,7 +781,8 @@ class State:
             t: TradeExecution
             for t in p.trades.values():
                 if t.is_unfinished():
-                    raise UncleanState(f"Position {p}, trade {t} is unfinished")
+                    tx_hashes = ", ".join([tx.hash for tx in t.blockchain_transactions])
+                    raise UncleanState(f"Position {p}, trade {t} is unfinished\nTransactions are: {tx_hashes}")
 
     def to_json_safe(self) -> str:
         """Serialise to JSON format with helpful validation and error messages.
