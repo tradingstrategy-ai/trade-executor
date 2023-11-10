b46bc94d4606789deed73f440844abfa23a74f69
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-11-11 00:13:00 +0100

    Finish Uniswap v3 momentum analysis

--- scripts/prepare-polygon-uniswap-v3-dataset.py
+++ scripts/prepare-polygon-uniswap-v3-dataset.py
@@ -1,17 +1,23 @@
 """Prepare a filtered Parquet file containing candles for all pairs on Uniswap v3 on Polygon."""
 import os
 
-from tradeexecutor.strategy.execution_context import ExecutionContext, python_script_execution_context
+from tradeexecutor.strategy.execution_context import python_script_execution_context
 from tradeexecutor.strategy.trading_strategy_universe import load_all_data
 from tradeexecutor.strategy.universe_model import UniverseOptions
 from tradingstrategy.chain import ChainId
 from tradingstrategy.client import Client
 from tradingstrategy.timebucket import TimeBucket
+from tradingstrategy.utils.forward_fill import forward_fill
+from tradingstrategy.utils.groupeduniverse import fix_bad_wicks
 
 client = Client.create_jupyter_client()
 
+chain_id = ChainId.polygon
+time_bucket = TimeBucket.d1
+exchange_slug = "uniswap-v3"
+
 exchanges = client.fetch_exchange_universe()
-uni = exchanges.get_by_chain_and_slug(ChainId.polygon, "uniswap-v3")
+uni = exchanges.get_by_chain_and_slug(ChainId.polygon, exchange_slug)
 
 dataset = load_all_data(
     client,
@@ -21,17 +27,30 @@ dataset = load_all_data(
     with_liquidity=False,
 )
 
+# Filter out pair ids that belong to our target dataset
+pair_universe = dataset.pairs
+pair_ids = pair_universe.loc[pair_universe["exchange_id"] == uni.exchange_id]["pair_id"]
+filtered_df = dataset.candles.loc[dataset.candles["pair_id"].isin(pair_ids)]
 
-pair_df = dataset.pairs
-pair_ids = pair_df.loc[pair_df["exchange_id"] == uni.exchange_id]["pair_id"]
+# Forward fill data
+filtered_df = filtered_df.set_index("timestamp")
 
-df = dataset.candles
+# Sanitise price data
+filtered_df = fix_bad_wicks(filtered_df)
 
-filtered_df = df.loc[df["pair_id"].isin(pair_ids)]
+# Make sure there are no gaps in the data
+filtered_df = filtered_df.groupby("pair_id")
+pairs_df = forward_fill(
+    filtered_df,
+    freq=time_bucket.to_frequency(),
+    columns=("open", "high", "low", "close", "volume"),
+)
 
-fpath = "/tmp/polygon-uni-v3-candles.parquet"
-
-filtered_df.to_parquet(fpath)
-
+# Wrote Parquest file under /tmp
+fpath = f"/tmp/{chain_id.get_slug()}-{exchange_slug}-candles-{time_bucket.value}.parquet"
+flattened_df = pairs_df.obj
+flattened_df = flattened_df.reset_index().set_index("timestamp")  # Get rid of grouping
+flattened_df.to_parquet(fpath)
 print(f"Wrote {fpath} {os.path.getsize(fpath):,} bytes")
+
 
