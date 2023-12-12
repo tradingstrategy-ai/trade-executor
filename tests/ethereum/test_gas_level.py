be6980f1e06bdbaf967308766ddab8b353bcf770
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-12-12 17:19:55 +0100

    Issue gas level warnings

---
+++ tests/ethereum/test_gas_level.py
@@ -0,0 +1,126 @@
+"""Gas level warning tests."""
+
+import secrets
+from decimal import Decimal
+
+import pytest
+from eth_account import Account
+from eth_defi.provider.anvil import launch_anvil, AnvilLaunch
+from eth_defi.chain import install_chain_middleware
+from eth_defi.hotwallet import HotWallet
+from eth_defi.trace import assert_transaction_success_with_explanation
+from eth_typing import HexAddress
+from hexbytes import HexBytes
+
+from web3 import Web3, HTTPProvider
+
+from tradeexecutor.ethereum.wallet import perform_gas_level_checks
+from tradeexecutor.strategy.run_state import RunState
+
+
+@pytest.fixture()
+def anvil() -> AnvilLaunch:
+
+    # London hardfork will enable EIP-1559 style gas fees
+    anvil = launch_anvil(
+        hardfork="london",
+        gas_limit=15_000_000,  # Max 5M gas per block, or per transaction in test automining
+    )
+    try:
+        # Make the initial snapshot ("zero state") to which we revert between tests
+        # web3 = Web3(HTTPProvider(anvil.json_rpc_url))
+        # snapshot_id = make_anvil_custom_rpc_request(web3, "evm_snapshot")
+        # assert snapshot_id == "0x0"
+        yield anvil
+    finally:
+        anvil.close()
+
+
+@pytest.fixture()
+def web3(anvil: AnvilLaunch) -> Web3:
+    """Set up the Anvil Web3 connection.
+
+    Also perform the Anvil state reset for each test.
+    """
+    web3 = Web3(HTTPProvider(anvil.json_rpc_url, request_kwargs={"timeout": 2}))
+    install_chain_middleware(web3)
+    return web3
+
+
+@pytest.fixture()
+def deployer(web3) -> HexAddress:
+    """Deployer account.
+
+    - This account will deploy all smart contracts
+
+    - Starts with 10,000 ETH
+    """
+    return web3.eth.accounts[0]
+
+
+@pytest.fixture()
+def user_1(web3) -> HexAddress:
+    """User account.
+
+    Do some account allocation for tests.
+    """
+    return web3.eth.accounts[1]
+
+
+@pytest.fixture
+def hot_wallet(web3, deployer, user_1) -> HotWallet:
+    """Create hot wallet for the signing tests.
+
+    Top is up with some gas money and 500 USDC.
+    """
+    private_key = HexBytes(secrets.token_bytes(32))
+    account = Account.from_key(private_key)
+    wallet = HotWallet(account)
+    wallet.sync_nonce(web3)
+    tx_hash = web3.eth.send_transaction({"to": wallet.address, "from": user_1, "value": 15 * 10**18})
+    assert_transaction_success_with_explanation(web3, tx_hash)
+    return wallet
+
+
+def test_hot_wallet_gas_level_warning_set(
+    web3: Web3,
+    hot_wallet: HotWallet,
+    deployer: HexAddress,
+):
+    """Hot wallet gas warning is set."""
+
+    assert hot_wallet.get_native_currency_balance(web3) == Decimal(15)
+
+    # Warning level 1000 ETH
+    run_state = RunState()
+    run_state.hot_wallet_gas_warning_level = 1000 * 10**18
+
+    flag = perform_gas_level_checks(
+        web3,
+        run_state,
+        hot_wallet,
+    )
+
+    assert flag
+    assert run_state.hot_wallet_gas_warning_message
+
+
+def test_hot_wallet_gas_level_warning_not_set(
+    web3: Web3,
+    hot_wallet: HotWallet,
+    deployer: HexAddress,
+):
+    """Hot wallet gas warning is set."""
+
+    # Warning level 0.1 ETH
+    run_state = RunState()
+    run_state.hot_wallet_gas_warning_level = Decimal(0.1)
+
+    flag = perform_gas_level_checks(
+        web3,
+        run_state,
+        hot_wallet,
+    )
+
+    assert not flag
+    assert run_state.hot_wallet_gas_warning_message is None
--- tradeexecutor/ethereum/wallet.py
+++ tradeexecutor/ethereum/wallet.py
@@ -4,7 +4,7 @@ import logging
 import dataclasses
 import datetime
 from decimal import Decimal
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from dataclasses_json import dataclass_json
 from eth_typing import HexAddress
@@ -13,9 +13,11 @@ from web3.types import BlockIdentifier
 
 from eth_defi.balances import DecimalisedHolding, \
     fetch_erc20_balances_by_token_list, convert_balances_to_decimal
+from eth_defi.hotwallet import HotWallet
 from eth_defi.provider.broken_provider import get_almost_latest_block_number
 from tradeexecutor.state.reserve import ReservePosition
 from tradeexecutor.state.identifier import AssetIdentifier
+from tradeexecutor.strategy.run_state import RunState
 
 logger = logging.getLogger(__name__)
 
@@ -122,5 +124,48 @@ def sync_reserves(
             logger.info("Reserve currency update detected. Asset: %s, past: %s, new: %s", evt.asset, evt.past_balance, evt.new_balance)
 
     return events
+
+
+def perform_gas_level_checks(
+    web3: Web3,
+    run_state: RunState,
+    hot_wallet: HotWallet,
+    hot_wallet_gas_warning_level: Optional[Decimal] = None,
+) -> bool:
+    """Check the gas level of the hot wallet.
+
+    - We need gas money to perform transactions
+
+    - Issue a low gas warnign if gas is running low
+
+    - Print a log message
+
+    - Set :py:class:`RunState`` flag.
+
+    - Does not do anything if warning gas level is not set
+
+    :param hot_wallet_gas_warning_level:
+        Set this or one in ``RunState.hot_wallet_gas_warning_level``.
+
+    :return:
+        True if the gas warning is going
+    """
+
+    hot_wallet_gas_warning_level = hot_wallet_gas_warning_level or run_state.hot_wallet_gas_warning_level
+
+    raw_gas = web3.eth.get_balance(hot_wallet.address)
+    gas = raw_gas / (10**18)
+
+    run_state.hot_wallet_gas = gas
+
+    if hot_wallet_gas_warning_level is not None:
+        if gas < hot_wallet_gas_warning_level:
+            gas_warn_message = f"Hot wallet address {hot_wallet.address}, gas is {gas} tokens, warning level is {hot_wallet_gas_warning_level}"
+            logger.warning(gas_warn_message)
+            run_state.hot_wallet_gas_warning_message = gas_warn_message
+            return True
+
+    return False
+
 
 
