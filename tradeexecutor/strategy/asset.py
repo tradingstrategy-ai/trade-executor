1e186ad6b8c9d9d2e229a20ff3fdcaec2727b09e
Mikko Ohtamaa <mikko@redinnovation.com>
2023-12-15 19:14:15 +0200

    Fix calculate_quantity_usd_value (#720)
    - `calculate_quantity_usd_value()` did not function correctly for a position without any good price data, prevented running `correct_accounts`
    - When running `correct_accounts` associate any token without a position to the last closed position, not the first one

--- tradeexecutor/strategy/asset.py
+++ tradeexecutor/strategy/asset.py
@@ -222,7 +222,9 @@ def build_expected_asset_map(
     # Closed positions appear in the position list only if there is not existing position,
     # otherwise all closed positions would get enumerated one by one
     #
-    for p in portfolio.closed_positions.values():
+    closed_positions_lifo = list(portfolio.closed_positions.values())
+    closed_positions_lifo.reverse()
+    for p in closed_positions_lifo:
         for asset, amount in get_asset_amounts(p):
             if asset not in mappings:
                 mappings[asset] = AssetToPositionsMapping(asset=asset)
