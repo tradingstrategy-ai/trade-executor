323ec8580d54d036cddbb744b4f4132cff235949
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-12-21 13:02:55 +0200

    Allow chartvisualisations to fail gracefully

--- tradeexecutor/strategy/pandas_trader/runner.py
+++ tradeexecutor/strategy/pandas_trader/runner.py
@@ -122,32 +122,39 @@ class PandasTraderRunner(StrategyRunner):
             logger.info("Strategy universe is empty - nothing to report")
             return
 
-        if pair_count == 1:
+        try:
 
-            small_figure = draw_single_pair_strategy_state(state, universe, height=512)
-            # Draw the inline plot and expose them tot he web server
-            # TODO: SVGs here are not very readable, have them as a stop gap solution
-            large_figure = draw_single_pair_strategy_state(state, universe, height=1024)
+            if pair_count == 1:
 
-            self.update_strategy_thinking_image_data(small_figure, large_figure)
+                small_figure = draw_single_pair_strategy_state(state, universe, height=512)
+                # Draw the inline plot and expose them tot he web server
+                # TODO: SVGs here are not very readable, have them as a stop gap solution
+                large_figure = draw_single_pair_strategy_state(state, universe, height=1024)
 
-        elif 1 < pair_count <= 3:
-            
-            small_figure_combined = draw_multi_pair_strategy_state(state, universe, height=1024)
-            large_figure_combined = draw_multi_pair_strategy_state(state, universe, height=2048)
+                self.update_strategy_thinking_image_data(small_figure, large_figure)
 
-            self.update_strategy_thinking_image_data(small_figure_combined, large_figure_combined)
+            elif 1 < pair_count <= 3:
 
-        elif 3 < pair_count <=5:
+                small_figure_combined = draw_multi_pair_strategy_state(state, universe, height=1024)
+                large_figure_combined = draw_multi_pair_strategy_state(state, universe, height=2048)
 
-            small_figure_combined = draw_multi_pair_strategy_state(state, universe, height=2048, detached_indicators = False)
-            large_figure_combined = draw_multi_pair_strategy_state(state, universe, height=3840, width = 2160, detached_indicators = False)
+                self.update_strategy_thinking_image_data(small_figure_combined, large_figure_combined)
 
-            self.update_strategy_thinking_image_data(small_figure_combined, large_figure_combined)
+            elif 3 < pair_count <=5:
 
-        else:
-            logger.warning("Charts not yet available for this strategy type. Pair count: %d", pair_count)
-    
+                small_figure_combined = draw_multi_pair_strategy_state(state, universe, height=2048, detached_indicators = False)
+                large_figure_combined = draw_multi_pair_strategy_state(state, universe, height=3840, width = 2160, detached_indicators = False)
+
+                self.update_strategy_thinking_image_data(small_figure_combined, large_figure_combined)
+
+            else:
+                logger.warning("Charts not yet available for this strategy type. Pair count: %d", pair_count)
+
+        except Exception as e:
+            # Don't take trade executor down if visualisations fail
+            logger.error("Could not draw visualisations")
+            logger.exception(e)
+
     def update_strategy_thinking_image_data(self, small_figure, large_figure):
         """Update the strategy thinking image data with small, small dark theme, large, and large dark theme images.
         
