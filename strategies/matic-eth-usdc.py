ecd92d995cdab5770bfa1a6ad614320ec093fd10
Mikko Ohtamaa <mikko@opensourcehacker.com>
2024-03-19 22:28:45 +0100

    Bump

--- strategies/matic-eth-usdc.py
+++ strategies/matic-eth-usdc.py
@@ -11,6 +11,7 @@ To backtest this strategy module locally:
         --strategy-file=strategies/matic-eth-usdc.py \
         --trading-strategy-api-key=$TRADING_STRATEGY_API_KEY
 
+To see the backtest for longer history, refer to the notebook doing backtest with Binance data.
 """
 import datetime
 
