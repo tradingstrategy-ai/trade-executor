5896e1b4fee3a56cbf12334911484cb9a4badbbb
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2024-03-20 19:24:58 +0200

    add time in market statistic (#842)
    ### Background
    
    - fixes #828 
    
    ### Summary
    
    - Adds manual implementation for `time in market` metric and adds it to summary stats
    - Avoids double counting so it still works for strategies that can have multiple open positions at once

--- tests/backtest/test_backtest_mixed_positions.py
+++ tests/backtest/test_backtest_mixed_positions.py
@@ -242,6 +242,11 @@ def test_backtest_long_short_stats(
     assert summary.compounding_returns.equals(overall_compounding_profit)
     assert long_summary.compounding_returns.equals(long_compounding_profit)
     assert short_summary.compounding_returns.equals(short_compounding_profit)
+
+    # all longs and shorts opened and closed at same time
+    assert summary.time_in_market == pytest.approx(0.5263157894736842)
+    assert long_summary.time_in_market == pytest.approx(0.5263157894736842)
+    assert short_summary.time_in_market == pytest.approx(0.5263157894736842)
     
     assert summary.return_percent == pytest.approx(overall_compounding_profit.iloc[-1], abs=1e-3)  # TODO make more precise
     assert summary.total_interest_paid_usd == pytest.approx(2.4602719114723985)
