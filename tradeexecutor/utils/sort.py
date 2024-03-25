c8e21a716ecb15cfcdc476e865214864c7b267df
Mikko Ohtamaa <mikko@opensourcehacker.com>
2024-03-25 13:07:02 +0100

    Add Pandas NA filter to unique_sort

--- tradeexecutor/utils/sort.py
+++ tradeexecutor/utils/sort.py
@@ -1,8 +1,10 @@
 """Sort helpers."""
 from typing import Any, Callable
 
+import pandas as pd
 
-def unique_sort(items: list, key: Callable, reverse=False) -> list[Any]:
+
+def unique_sort(items: list, key: Callable, reverse=False, filter_na=True) -> list[Any]:
     """Sort the list, but return the unique results only.
 
     - Similar as :py:func:`sorted`
@@ -21,12 +23,21 @@ def unique_sort(items: list, key: Callable, reverse=False) -> list[Any]:
     :param reverse:
         Reverse the result
 
+    :param filter_na:
+        If the sorted value is NA, ignore it.
+
+        Use `pandas.isna` to detect.
     """
     accessed = ((key(item), item) for item in items)
     first_pass = sorted(accessed, key=lambda x: x[0], reverse=reverse)
     result = []
     uniq = set()
     for key, item in first_pass:
+
+        if filter_na:
+            if pd.isna(key):
+                continue
+
         if key not in uniq:
             uniq.add(key)
             result.append(item)
