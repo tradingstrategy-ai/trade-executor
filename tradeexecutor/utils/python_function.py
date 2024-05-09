28ed3f198620696067b0635ac0a9debdf7d747f9
Mikko Ohtamaa <mikko@opensourcehacker.com>
2024-05-09 11:43:26 +0200

    Fix hash_function for cloudpickle

--- tradeexecutor/utils/python_function.py
+++ tradeexecutor/utils/python_function.py
@@ -32,6 +32,9 @@ def hash_function(func, char_length=8):
 
     See https://stackoverflow.com/a/49998190/315168
 
+    Hash is not guaranteed to be the same across different environments / scenarios.
+    Read the function for more info.
+
     :param char_length:
         How many characters you want in your hash,
         to reduce the hash size.
@@ -42,32 +45,41 @@ def hash_function(func, char_length=8):
 
     assert callable(func), f"Not a function: {func}"
 
-    func_str = inspect.getsource(func)
+    try:
+        func_str = inspect.getsource(func)
+        func_str = textwrap.dedent(func_str)
+        # Heurestics if this is a lambda function - in this case ast will fail
+        # Account for ending new line (may or may not be there?)
+        lambda_like = len(func_str.split("\n")) in (1, 2) and "lambda" in func_str
 
-    func_str = textwrap.dedent(func_str)
+        if not lambda_like:
+            module = ast.parse(func_str)
+            assert len(module.body) == 1 and isinstance(module.body[0], ast.FunctionDef)
 
-    # Heurestics if this is a lambda function - in this case ast will fail
-    # Account for ending new line (may or may not be there?)
-    lambda_like = len(func_str.split("\n")) in (1, 2) and "lambda" in func_str
+            # Clear function name so it doesn't affect the hash
+            func_node = module.body[0]
+            func_node.name = ""
 
-    if not lambda_like:
-        module = ast.parse(func_str)
-        assert len(module.body) == 1 and isinstance(module.body[0], ast.FunctionDef)
+            # Clear all the doc strings
+            for node in ast.walk(module):
+                _remove_docstring(node)
 
-        # Clear function name so it doesn't affect the hash
-        func_node = module.body[0]
-        func_node.name = ""
+            # Convert the ast to a string for hashing
+            ast_str = ast.dump(module, annotate_fields=False).encode("utf-8")
+            # Produce the hash
+            fhash = hashlib.sha256(ast_str)
+        else:
+            # Handle lambda special case
+            fhash = hashlib.sha256(func_str.encode("utf-8"))
 
-        # Clear all the doc strings
-        for node in ast.walk(module):
-            _remove_docstring(node)
-
-        # Convert the ast to a string for hashing
-        ast_str = ast.dump(module, annotate_fields=False).encode("utf-8")
-        # Produce the hash
-        fhash = hashlib.sha256(ast_str)
-    else:
-        # Handle lambda special case
-        fhash = hashlib.sha256(func_str.encode("utf-8"))
+    except OSError:
+        # When using https://github.com/cloudpipe/cloudpickle with multiprocessing.
+        # Fall back to function bytecode
+        # File "/Users/moo/.pyenv/versions/3.11.5/lib/python3.11/inspect.py", line 1081, in findsource
+        #   raise OSError('could not get source code')
+        fhash = hashlib.sha256(func.__code__.co_code)
 
     return fhash.hexdigest()[0:char_length]
+
+
+
