856b73f7d5de87d643c15fdfc9c0e2815e9c6dd2
Mikko Ohtamaa <mikko@opensourcehacker.com>
2024-07-29 23:42:52 +0900

    Adding a test for gradually increase credit

--- tests/strategy_tests/test_increase_credit.py
+++ tests/strategy_tests/test_increase_credit.py
@@ -1,48 +1,35 @@
+"""Gradully increase credit position when new cash is deposited in."""
 
 import datetime
-import logging
-import os
 import random
 from decimal import Decimal
-from pathlib import Path
 from typing import List
 
 import pytest
-import pandas as pd
 
-from tradeexecutor.backtest.backtest_routing import BacktestRoutingModel
-from tradeexecutor.backtest.backtest_runner import run_backtest, setup_backtest_for_universe, run_backtest_inline
+from tradeexecutor.backtest.backtest_runner import run_backtest_inline
 from tradeexecutor.backtest.backtest_sync import BacktestSyncModel
-from tradeexecutor.cli.log import setup_pytest_logging
 from tradeexecutor.cli.loop import ExecutionTestHook
 from tradeexecutor.state.identifier import AssetIdentifier, TradingPairIdentifier
 from tradeexecutor.state.state import State
 from tradeexecutor.state.trade import TradeExecution
-from tradeexecutor.statistics.summary import calculate_summary_statistics
 from tradeexecutor.strategy.cycle import CycleDuration
 from tradeexecutor.strategy.execution_context import ExecutionMode, unit_test_execution_context, ExecutionContext
 from tradeexecutor.strategy.pandas_trader.indicator import DiskIndicatorStorage, IndicatorSet
 from tradeexecutor.strategy.pandas_trader.strategy_input import StrategyInput
 from tradeexecutor.strategy.parameters import StrategyParameters
-from tradeexecutor.strategy.trading_strategy_universe import TradingStrategyUniverse, create_pair_universe_from_code
-from tradeexecutor.strategy.universe_model import UniverseOptions, default_universe_options
+from tradeexecutor.strategy.trading_strategy_universe import TradingStrategyUniverse
 from tradeexecutor.testing.synthetic_ethereum_data import generate_random_ethereum_address
-from tradeexecutor.testing.synthetic_exchange_data import generate_exchange, generate_simple_routing_model
+from tradeexecutor.testing.synthetic_exchange_data import generate_exchange
 from tradeexecutor.testing.synthetic_lending_data import generate_lending_reserve, generate_lending_universe
 from tradeexecutor.testing.synthetic_price_data import generate_ohlcv_candles
 from tradeexecutor.testing.synthetic_universe_data import create_synthetic_single_pair_universe
-from tradeexecutor.visual.equity_curve import calculate_investment_flow, calculate_realised_profitability, calculate_deposit_adjusted_returns, \
-    calculate_compounding_realised_trading_profitability, calculate_size_relative_realised_trading_returns, calculate_cumulative_daily_returns, calculate_non_cumulative_daily_returns
-from tradingstrategy.candle import GroupedCandleUniverse
 from tradingstrategy.chain import ChainId
-from tradingstrategy.exchange import Exchange
 from tradingstrategy.timebucket import TimeBucket
-from tradingstrategy.universe import Universe
-
 
 
 class DepositSimulator(ExecutionTestHook):
-    """Do FizzBuzz deposits/redemptions simulation."""
+    """Each day, randomly add deposit some more cash to the strategy."""
 
     def __init__(self):
         self.deposit_callbacks_done = 0
@@ -66,7 +53,7 @@ class DepositSimulator(ExecutionTestHook):
             sync_model.simulate_funding(fund_event_ts, Decimal(100))
 
         if cycle % 5 == 0:
-            sync_model.simulate_funding(fund_event_ts, Decimal(-90))
+            sync_model.simulate_funding(fund_event_ts, Decimal(90))
 
         self.deposit_callbacks_done += 1
 
@@ -136,8 +123,13 @@ def decide_trades(input: StrategyInput) -> List[TradeExecution]:
     """A simple strategy that puts all in to our lending reserve."""
 
     position_manager = input.get_position_manager()
-    pair = input.get_default_pair()
     cash = input.state.portfolio.get_cash()
+    strategy_universe = input.strategy_universe
+    credit_supply_pair = strategy_universe.get_credit_supply_pair()
+
+    # Since first cycle we should have a credit supply position open
+    if input.cycle >= 2:
+        assert position_manager.get_current_position_for_pair(credit_supply_pair) is not None
 
     trades = position_manager.open_credit_supply_position_for_reserves(
         cash * 0.98,
@@ -176,3 +168,12 @@ def test_increase_credit(
         indicator_storage=indicator_storage,
         execution_test_hook=deposit_simulator,
     )
+
+    state = result.state
+    portfolio = state.portfolio
+    assert len(portfolio.open_positions) == 1
+
+    position = next(iter(portfolio.open_positions.values()))
+    assert position.is_credit_supply()
+    assert len(position.trades) == 151
+
