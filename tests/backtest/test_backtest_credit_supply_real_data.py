efe9e7f85c3d6364c6c1c5277a851544e864d9ab
Hieu <hieu@tradingstrategy.ai>
2024-09-09 11:38:46 +0300

    Force loading lending candles time bucket to be 1h (#1039)
    Currently lending candles are loaded with same time bucket as the main trading data bucket.
    Since the purpose of lending candles is totally different, mainly for calculating interest rate, we should get it as realtime as posisble
    
    Fix #1037

--- tests/backtest/test_backtest_credit_supply_real_data.py
+++ tests/backtest/test_backtest_credit_supply_real_data.py
@@ -115,10 +115,10 @@ def test_backtest_open_only_credit_supply_real_data(
     assert len(credit_position.balance_updates) == 30
     
     assert credit_position.loan.collateral_interest.opening_amount == Decimal("10000.00")
-    assert credit_position.get_accrued_interest() == pytest.approx(5.8392619598023785)
-    assert credit_position.get_quantity() == pytest.approx(Decimal('10005.83926195980237824844194'))
-    assert credit_position.get_value() == pytest.approx(10005.8392619598023785)
-    assert portfolio.get_total_equity() == pytest.approx(10005.8392619598023785)
+    assert credit_position.get_accrued_interest() == pytest.approx(5.777347790050936)
+    assert credit_position.get_quantity() == pytest.approx(Decimal(10005.77734779005093525703117))
+    assert credit_position.get_value() == pytest.approx(10005.77734779005093525703117)
+    assert portfolio.get_total_equity() == pytest.approx(10005.77734779005093525703117)
 
 
 def test_backtest_open_and_close_credit_supply_real_data(
@@ -179,9 +179,9 @@ def test_backtest_open_and_close_credit_supply_real_data(
 
     interest = credit_position.loan.collateral_interest
     assert interest.opening_amount == Decimal("10000.00")
-    assert credit_position.calculate_accrued_interest_quantity(credit_position.loan.collateral.asset) == pytest.approx(Decimal('2.67867669078453209820818'))
+    assert credit_position.calculate_accrued_interest_quantity(credit_position.loan.collateral.asset) == pytest.approx(Decimal(2.67988628793577073776570))
     assert interest.last_token_amount == pytest.approx(Decimal('0'))
-    assert interest.last_accrued_interest == pytest.approx(Decimal('2.67867669078453209820818'))
+    assert interest.last_accrued_interest == pytest.approx(Decimal(2.67988628793577073776570))
 
     assert credit_position.get_accrued_interest() == Decimal(0)  # Accrued interest should be 0 for closed position
     assert credit_position.get_quantity() == Decimal('0')  # Closed positions do not have quantity left
