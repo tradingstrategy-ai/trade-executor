53c6d81d4c45ae66b9dc96081bbcdaaab00f6cc4
Hieu Nguyen <hieu@tradingstrategy.ai>
2024-09-10 10:47:08 +0300

    Update release script to avoid double tagging same commit

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,11 +10,23 @@
 
 #: Github repo id
 REPO=tradingstrategy-ai/trade-executor
+RELEASE_BRANCH=master
 
 set -e
 
-# Get tags locally
-git fetch --all
+echo "Pulling latest $RELEASE_BRANCH and all tags"
+git checkout $RELEASE_BRANCH
+git pull --all
+
+echo "Latest commit is: `git log --oneline -1`"
+
+latest_commit_id=`git rev-parse HEAD`
+linked_tag=`git tag --points-at $latest_commit_id`
+
+if [[ $linked_tag ]]; then
+    echo "Latest commit already references a tag: $linked_tag => no new release"
+    exit
+fi
 
 # https://gist.github.com/rponte/fdc0724dd984088606b0
 latest_tag=`git tag --sort=committerdate | tail -1`
@@ -29,8 +41,6 @@ build_url="https://github.com/$REPO/pkgs/container/trade-executor"
 let "new_version = $latest_version + 1"
 
 new_tag="v$new_version"
-
-echo "Latest commit is $latest_commit"
 
 # https://stackoverflow.com/a/3232082/315168
 read -r -p "New tag is $new_tag - make a release? [y/N] " response
