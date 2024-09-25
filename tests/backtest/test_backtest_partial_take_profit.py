b39f362fcacb79a160806f97d92fb08c6a27a6e7
Hieu Nguyen <hieu@tradingstrategy.ai>
2024-09-26 01:09:59 +0300

    Add more test case

--- tests/backtest/test_backtest_partial_take_profit.py
+++ tests/backtest/test_backtest_partial_take_profit.py
@@ -85,7 +85,7 @@ def strategy_universe() -> TradingStrategyUniverse:
         start_at,
         end_at,
         pair_id=weth_usdc.internal_id,
-        daily_drift=(1.01, 1.01),
+        daily_drift=(0.99, 1.02),
         high_drift=1.05,
         low_drift=0.90,
         random_seed=1,
@@ -134,8 +134,8 @@ def create_indicators(timestamp: datetime.datetime, parameters: StrategyParamete
     return IndicatorSet()
 
 
-def decide_trades(input: StrategyInput) -> List[TradeExecution]:
-    """Example decide_trades function using market limits and partial take profits."""
+def decide_trades_1(input: StrategyInput) -> List[TradeExecution]:
+    """Example decide_trades function using partial take profits."""
     position_manager = input.get_position_manager()
     pair = input.get_default_pair()
     cash = input.state.portfolio.get_cash()
@@ -162,8 +162,8 @@ def decide_trades(input: StrategyInput) -> List[TradeExecution]:
             total_quantity = position.get_quantity()
 
             tp1_price = opening_price * 1.1
-            tp2_price = opening_price * 1.2
-            tp3_price = opening_price * 1.5
+            tp2_price = opening_price * 1.15
+            tp3_price = opening_price * 1.2
 
             tp1_quantity = -total_quantity * Decimal(0.4)
             tp2_quantity = -total_quantity * Decimal(0.3)
@@ -186,7 +186,6 @@ def decide_trades(input: StrategyInput) -> List[TradeExecution]:
         trades = position_manager.open_spot(
             pair=pair,
             value=cash * 0.99,
-            stop_loss_pct=parameters.stop_loss_pct,
         )
 
         return trades
@@ -197,23 +196,86 @@ def decide_trades(input: StrategyInput) -> List[TradeExecution]:
     return []
 
 
-def test_partial_take_profit(strategy_universe, tmp_path):
-    """Test DecideTradesProtocolV4
+def decide_trades_2(input: StrategyInput) -> List[TradeExecution]:
+    """Example decide_trades function using partial take profits and trailing stoploss."""
+    position_manager = input.get_position_manager()
+    pair = input.get_default_pair()
+    cash = input.state.portfolio.get_cash()
+    indicators = input.indicators
+    portfolio = input.state.portfolio
+    parameters = input.parameters
 
-    - Check that StrategyInput is passed correctly in backtesting (only backtesting, not live trading)
+    price = indicators.get_price()
+    print("Cycle", input.cycle, price)
+    if price is None:
+        # Skip cycle 1
+        # We do not have the previous day price available at the first cycle
+        return []
+    
+    for position in portfolio.open_positions.values():
+        # Check if the position already has take profit triggers in pending trades
+        has_take_profit_triggers = any(
+            any(trigger.type == TriggerType.take_profit_partial for trigger in trade.triggers)
+            for trade in position.pending_trades.values()
+        )
+        
+        if not has_take_profit_triggers:
+            print("Setting partial tp")
+            opening_price = position.get_opening_price()
+
+            total_quantity = position.get_quantity()
+
+            tp1_price = opening_price * 1.1
+            tp2_price = opening_price * 1.15
+            tp3_price = opening_price * 2
+
+            tp1_quantity = -total_quantity * Decimal(0.4)
+            tp2_quantity = -total_quantity * Decimal(0.3)
+
+            # NOTE: intentionally set tp3_quantity to be larger than it should
+            # but the test should still pass as this amount should be ignored for closing trade
+            tp3_quantity = -total_quantity * Decimal(0.9)
+
+            position_manager.prepare_take_profit_trades(
+                position,
+                [
+                    (float(tp1_price), tp1_quantity, False), 
+                    (float(tp2_price), tp2_quantity, False),  
+                    (float(tp3_price), tp3_quantity, True),   
+                ]
+            )
+            
+    # Only set a trigger open if we do not have any position open/pending yet
+    if not position_manager.get_current_position_for_pair(pair, pending=True):
+        trades = position_manager.open_spot(
+            pair=pair,
+            value=cash * 0.99,
+            trailing_stop_loss_pct=0.98,
+        )
+
+        return trades
+
+    # We return zero trades here, as all of trades we have constructed
+    # are pending for a trigger, and do not need to be executed
+    # on this decision cycle
+    return []
+
+
+def test_partial_take_profit(strategy_universe):
+    """Test partial take profit works and last trade fully close the position 
+    with correct quantity
     """
 
     class Parameters:
         backtest_start = strategy_universe.data_universe.candles.get_timestamp_range()[0].to_pydatetime()
-        backtest_end = strategy_universe.data_universe.candles.get_timestamp_range()[0].to_pydatetime()  + datetime.timedelta(days=4)
+        backtest_end = strategy_universe.data_universe.candles.get_timestamp_range()[0].to_pydatetime()  + datetime.timedelta(days=3)
         initial_cash = 100_000
         cycle_duration = CycleDuration.cycle_1d
-        stop_loss_pct = 0.95
 
     # Run the test
     result = run_backtest_inline(
         client=None,
-        decide_trades=decide_trades,
+        decide_trades=decide_trades_1,
         create_indicators=create_indicators,
         universe=strategy_universe,
         reserve_currency=ReserveCurrency.usdc,
@@ -223,19 +285,67 @@ def test_partial_take_profit(strategy_universe, tmp_path):
     )
 
     state = result.state
-    assert len(result.diagnostics_data) == 6  # Entry for each day + few extras
+    assert len(result.diagnostics_data) == 5  # Entry for each day + few extras
     assert len(state.portfolio.closed_positions) == 1
     assert len(state.portfolio.pending_positions) == 0
-    assert len(state.portfolio.open_positions) == 1
+    assert len(state.portfolio.open_positions) == 0
 
-    
     closed_position = state.portfolio.closed_positions[1]
     assert len(closed_position.trades) == 4
 
     last_trade = closed_position.trades[4]
     # check the planned price is the trigger_price
-    assert last_trade.planned_price == pytest.approx(3438.5684016892783)
-    assert last_trade.executed_price == pytest.approx(3438.5684016892783)
+    assert last_trade.planned_quantity == pytest.approx(Decimal(-15.09447875566598427355031245))
+    assert last_trade.planned_price == pytest.approx(2361.1282361520357)
+    assert last_trade.executed_price == pytest.approx(2361.1282361520357)
+
+    # Check these do not crash on market limit positions
+    calculate_summary_statistics(
+        state,
+        ExecutionMode.unit_testing_trading,
+        now_=datetime.datetime.utcnow(),
+    )
+
+
+def test_partial_tp_and_trailing_sl(strategy_universe):
+    """Test partial take profit works and last trade fully close the position 
+    with correct quantity
+    """
+
+    class Parameters:
+        backtest_start = strategy_universe.data_universe.candles.get_timestamp_range()[0].to_pydatetime()
+        backtest_end = strategy_universe.data_universe.candles.get_timestamp_range()[0].to_pydatetime()  + datetime.timedelta(days=3)
+        initial_cash = 100_000
+        cycle_duration = CycleDuration.cycle_1d
+
+    # Run the test
+    result = run_backtest_inline(
+        client=None,
+        decide_trades=decide_trades_2,
+        create_indicators=create_indicators,
+        universe=strategy_universe,
+        reserve_currency=ReserveCurrency.usdc,
+        engine_version="0.5",
+        parameters=StrategyParameters.from_class(Parameters),
+        mode=ExecutionMode.unit_testing,
+    )
+
+    state = result.state
+    # assert len(result.diagnostics_data) == 6  # Entry for each day + few extras
+    assert len(state.portfolio.closed_positions) == 1
+    assert len(state.portfolio.pending_positions) == 0
+
+    
+    closed_position = state.portfolio.closed_positions[1]
+    assert len(closed_position.trades) == 2
+    assert closed_position.is_stop_loss()
+
+    trade = closed_position.trades[2]
+    print(trade.trade_type)
+    print(closed_position.trades[1], closed_position.trades[2])
+    # # check the planned price is the trigger_price
+    # assert last_trade.planned_price == pytest.approx(3438.5684016892783)
+    # assert last_trade.executed_price == pytest.approx(3438.5684016892783)
 
     # Check these do not crash on market limit positions
     calculate_summary_statistics(
