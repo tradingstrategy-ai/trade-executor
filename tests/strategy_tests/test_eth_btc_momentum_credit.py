44fa06905f1ced7cf8bd933cbed885478d1c6edb
Hieu Nguyen <hieu@tradingstrategy.ai>
2024-11-13 09:15:14 +0200

    Add more

---
+++ tests/strategy_tests/test_eth_btc_momentum_credit.py
@@ -0,0 +1,59 @@
+"""Run quick smoke test on eth-btc-usdc-credit strategy."""
+import os
+from pathlib import Path
+from unittest import mock
+
+import pytest
+
+from tradeexecutor.cli.commands.app import app
+from tradeexecutor.state.state import State
+
+
+@pytest.fixture()
+def strategy_file() -> Path:
+    """The strategy module where the broken accounting happened."""
+    p = Path(os.path.join(os.path.dirname(__file__), "..", "..", "strategies", "eth-btc-usdc-credit.py"))
+    assert p.exists(), f"{p.resolve()} missing"
+    return p
+
+
+@pytest.fixture()
+def state_file(tmp_path) -> Path:
+    """The strategy module where the broken accounting happened."""
+    return Path(tmp_path / "state.json")
+
+
+@pytest.fixture()
+def environment(
+    strategy_file: Path,
+    state_file: Path,
+) -> dict:
+    """Passed to init and start commands as environment variables"""
+    # Set up the configuration for the live trader
+    environment = {
+        "STRATEGY_FILE": strategy_file.as_posix(),
+        "STATE_FILE": state_file.as_posix(),
+        "ASSET_MANAGEMENT_MODE": "enzyme",
+        "UNIT_TESTING": "true",
+        "LOG_LEVEL": "disabled",
+        "TRADING_STRATEGY_API_KEY": os.environ["TRADING_STRATEGY_API_KEY"],
+        "USE_BINANCE": "false",  # Force to use DEX + lending data in the backtest
+    }
+    return environment
+
+
+def test_eth_btc_momentum_credit(
+    environment: dict,
+    state_file: Path,
+):
+    """Backtest the strategy with a short period.
+
+    - Ensure that trade size works with alpha model
+    """
+    with mock.patch.dict('os.environ', environment, clear=True):
+        app(["backtest"], standalone_mode=False)
+
+    state = State.read_json_file(state_file)
+    credit_positions = [p for p in state.portfolio.get_all_positions() if p.is_credit_supply()]
+    assert len(credit_positions) >= 15  # TODO: Why this number varies - calendar?
+
---
+++ tests/strategy_tests/test_long_short_momentum.py
@@ -0,0 +1,56 @@
+"""See tgat long-short-momentum.py does not crash."""
+import datetime
+import logging
+import os
+
+from pathlib import Path
+
+import pytest
+
+from tradeexecutor.analysis.trade_analyser import build_trade_analysis
+from tradeexecutor.backtest.backtest_runner import run_backtest, setup_backtest
+from tradeexecutor.cli.log import setup_pytest_logging
+from tradeexecutor.strategy.universe_model import UniverseOptions
+
+
+# https://docs.pytest.org/en/latest/how-to/skipping.html#skip-all-test-functions-of-a-class-or-module
+pytestmark = pytest.mark.skipif(os.environ.get("TRADING_STRATEGY_API_KEY") is None, reason="Set TRADING_STRATEGY_API_KEY environment variable to run this test")
+
+
+@pytest.fixture(scope="module")
+def logger(request):
+    """Setup test logger."""
+    return setup_pytest_logging(request, mute_requests=False)
+
+
+@pytest.fixture()
+def strategy_path() -> Path:
+    """Where do we load our strategy file."""
+    return Path(os.path.join(os.path.dirname(__file__), "..", "..", "strategies", "spot-and-short-momentum.py"))
+
+
+def test_long_short_momentum(
+    strategy_path,
+    logger: logging.Logger,
+    persistent_test_client,
+    ):
+    """Check the strategy does not crash."""
+
+    client = persistent_test_client
+
+    # Run backtest over 6 months, daily
+    setup = setup_backtest(
+        strategy_path,
+    )
+
+    try:
+        state, universe, debug_dump = run_backtest(setup, client)
+    except Exception as e:
+        logger.error("Backtest failed:\n%s", e)
+        logger.exception(e)
+        raise e
+
+    analysis = build_trade_analysis(state.portfolio)
+    summary = analysis.calculate_summary_statistics(universe.data_universe.time_bucket, state)
+    df = summary.to_dataframe()
+    str(df)
