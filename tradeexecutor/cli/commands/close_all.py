21ea1e3406257d6961f4fa84a3e6e5b047138de6
Mikko Ohtamaa <mikko@redinnovation.com>
2024-11-26 17:53:32 +0100

    Add slippage tolerance to close all

--- tradeexecutor/cli/commands/close_all.py
+++ tradeexecutor/cli/commands/close_all.py
@@ -173,6 +173,7 @@ def close_all(
         universe,
         runner.routing_model,
         routing_state,
+        slippage_tolerance=mod.parameters.get("slippage_tolerance") or 0.01,
         interactive=interactive,
     )
 
