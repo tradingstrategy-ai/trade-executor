61a422126a9d917ceef6cc1b9877f29b5f921c29
Mikko Ohtamaa <mikko@redinnovation.com>
2025-01-03 14:34:17 +0100

    Adding Lagoon integration models

---
+++ tests/lagoon/conftest.py
@@ -0,0 +1,233 @@
+"""Base mainnet fork based tests for Lagoon.
+
+Explore the static deployment which we fork from the Base mainnet:
+
+- Vault UI: https://trading-stategy-users-frontend.vercel.app/vault/8453/0xab4ac28d10a4bc279ad073b1d74bfa0e385c010c
+- Vault contract: https://basescan.org/address/0xab4ac28d10a4bc279ad073b1d74bfa0e385c010c#readProxyContract
+- Safe UI: https://app.safe.global/home?safe=base:0x20415f3Ec0FEA974548184bdD6e67575D128953F
+- Safe contract: https://basescan.org/address/0x20415f3Ec0FEA974548184bdD6e67575D128953F#readProxyContract
+- Roles: https://app.safe.global/apps/open?safe=base:0x20415f3Ec0FEA974548184bdD6e67575D128953F&appUrl=https%3A%2F%2Fzodiac.gnosisguild.org%2F
+"""
+import os
+
+import pytest
+from eth_typing import HexAddress
+from web3 import Web3
+
+from eth_defi.hotwallet import HotWallet
+from eth_defi.lagoon.vault import LagoonVault
+from eth_defi.provider.anvil import AnvilLaunch, fork_network_anvil
+from eth_defi.provider.multi_provider import create_multi_provider_web3
+from eth_defi.token import TokenDetails, fetch_erc20_details
+from eth_defi.trace import assert_transaction_success_with_explanation
+from eth_defi.vault.base import VaultSpec
+
+JSON_RPC_BASE = os.environ.get("JSON_RPC_BASE")
+
+CI = os.environ.get("CI", None) is not None
+
+pytestmark = pytest.mark.skipif(not JSON_RPC_BASE, reason="No JSON_RPC_BASE environment variable")
+
+
+@pytest.fixture()
+def vault_owner() -> HexAddress:
+    # Vaut owner
+    return "0x0c9db006f1c7bfaa0716d70f012ec470587a8d4f"
+
+
+@pytest.fixture()
+def depositor() -> HexAddress:
+    # Someone how deposited assets to the vault earlier
+    return "0x20415f3Ec0FEA974548184bdD6e67575D128953F"
+
+
+@pytest.fixture()
+def usdc_holder() -> HexAddress:
+    # https://basescan.org/token/0x833589fcd6edb6e08f4c7c32d4f71b54bda02913#balances
+    return "0x3304E22DDaa22bCdC5fCa2269b418046aE7b566A"
+
+
+@pytest.fixture()
+def valuation_manager() -> HexAddress:
+    """Unlockable account set as the vault valuation manager."""
+    return "0x8358bBFb4Afc9B1eBe4e8C93Db8bF0586BD8331a"
+
+
+@pytest.fixture()
+def safe_address() -> HexAddress:
+    """Unlockable Safe multisig as spoofed Anvil account."""
+    return "0x20415f3Ec0FEA974548184bdD6e67575D128953F"
+
+
+@pytest.fixture()
+def anvil_base_fork(request, vault_owner, usdc_holder, asset_manager, valuation_manager) -> AnvilLaunch:
+    """Create a testable fork of live BNB chain.
+
+    :return: JSON-RPC URL for Web3
+    """
+    assert JSON_RPC_BASE, "JSON_RPC_BASE not set"
+    launch = fork_network_anvil(
+        JSON_RPC_BASE,
+        unlocked_addresses=[vault_owner, usdc_holder, asset_manager, valuation_manager],
+    )
+    try:
+        yield launch
+    finally:
+        # Wind down Anvil process after the test is complete
+        launch.close()
+
+
+@pytest.fixture()
+def web3(anvil_base_fork) -> Web3:
+    """Create a web3 connector.
+
+    - By default use Anvil forked Base
+
+    - Eanble Tenderly testnet with `JSON_RPC_TENDERLY` to debug
+      otherwise impossible to debug Gnosis Safe transactions
+    """
+
+    tenderly_fork_rpc = os.environ.get("JSON_RPC_TENDERLY", None)
+
+    if tenderly_fork_rpc:
+        web3 = create_multi_provider_web3(tenderly_fork_rpc)
+    else:
+        web3 = create_multi_provider_web3(
+            anvil_base_fork.json_rpc_url,
+            default_http_timeout=(3, 250.0),  # multicall slow, so allow improved timeout
+        )
+    assert web3.eth.chain_id == 8453
+    return web3
+
+
+@pytest.fixture()
+def base_usdc(web3) -> TokenDetails:
+    return fetch_erc20_details(
+        web3,
+        "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+    )
+
+
+@pytest.fixture()
+def base_weth(web3) -> TokenDetails:
+    return fetch_erc20_details(
+        web3,
+        "0x4200000000000000000000000000000000000006",
+    )
+
+
+@pytest.fixture()
+def base_dino(web3) -> TokenDetails:
+    """A token that trades as DINO/WETH on Uniswap v2
+
+    https://app.uniswap.org/explore/pools/base/0x6a77CDeC82EFf6A6A5D273F18C1c27CD3d71A588
+    """
+    return fetch_erc20_details(
+        web3,
+        "0x85E90a5430AF45776548ADB82eE4cD9E33B08077",
+    )
+
+
+@pytest.fixture()
+def hot_wallet_user(web3, usdc, usdc_holder) -> HotWallet:
+    """A test account with USDC balance."""
+
+    hw = HotWallet.create_for_testing(
+        web3,
+        test_account_n=1,
+        eth_amount=10
+    )
+    hw.sync_nonce(web3)
+
+    # give hot wallet some native token
+    web3.eth.send_transaction(
+        {
+            "from": web3.eth.accounts[9],
+            "to": hw.address,
+            "value": 1 * 10**18,
+        }
+    )
+
+    # Top up with 999 USDC
+    tx_hash = usdc.contract.functions.transfer(hw.address, 999 * 10**6).transact({"from": usdc_holder, "gas": 100_000})
+    assert_transaction_success_with_explanation(web3, tx_hash)
+    return hw
+
+
+@pytest.fixture()
+def base_test_vault_spec() -> VaultSpec:
+    """Vault is 0xab4ac28d10a4bc279ad073b1d74bfa0e385c010c
+
+    - https://trading-stategy-users-frontend.vercel.app/vault/8453/0xab4ac28d10a4bc279ad073b1d74bfa0e385c010c
+    - https://app.safe.global/home?safe=base:0x20415f3Ec0FEA974548184bdD6e67575D128953F
+    """
+    return VaultSpec(1, "0xab4ac28d10a4bc279ad073b1d74bfa0e385c010c")
+
+
+@pytest.fixture()
+def lagoon_vault(web3, base_test_vault_spec: VaultSpec) -> LagoonVault:
+    return LagoonVault(web3, base_test_vault_spec)
+
+
+@pytest.fixture()
+def asset_manager() -> HexAddress:
+    """The asset manager role."""
+    return "0x0b2582E9Bf6AcE4E7f42883d4E91240551cf0947"
+
+
+@pytest.fixture()
+def topped_up_asset_manager(web3, asset_manager):
+    # Topped up with some ETH
+    tx_hash = web3.eth.send_transaction({
+        "to": asset_manager,
+        "from": web3.eth.accounts[0],
+        "value": 9 * 10**18,
+    })
+    assert_transaction_success_with_explanation(web3, tx_hash)
+    return asset_manager
+
+
+@pytest.fixture()
+def topped_up_valuation_manager(web3, valuation_manager):
+    # Topped up with some ETH
+    tx_hash = web3.eth.send_transaction({
+        "to": valuation_manager,
+        "from": web3.eth.accounts[0],
+        "value": 9 * 10**18,
+    })
+    assert_transaction_success_with_explanation(web3, tx_hash)
+    return valuation_manager
+
+
+
+@pytest.fixture()
+def lagoont_execution_model(
+    web3: Web3,
+    lagoon_vault: LagoonVault,
+) -> LagoonExecution:
+    """Set EthereumExecutionModel in Base fork testing mode."""
+
+    private_key = os.environ["VELVET_VAULT_OWNER_PRIVATE_KEY"]
+
+    assert not private_key.startswith("http"), f"Github WTF: {private_key}"
+
+    hot_wallet = HotWallet.from_private_key(private_key)
+    execution_model = LagoonExecution(
+        vault=lagoon_vault,
+        tx_builder=LagoonTransactionBuilder(lagoon_vault, hot_wallet),
+        mainnet_fork=True,
+        confirmation_block_count=0,
+    )
+    return execution_model
+
+
+@pytest.fixture()
+def lagoon_routing_model(base_weth, base_usdc) -> VelvetEnsoRouting:
+    """Create Enso routing model.
+    """
+
+    # Uses default router choose function
+    return VelvetEnsoRouting(
+        reserve_token_address=base_usdc.address,
+        allowed_intermediary_pairs={base_weth.address: "0xd0b53d9277642d899df5c87a3966a349a798f224"},  # WETH
+    )
---
+++ tests/lagoon/test_lagoon_swap.py
@@ -0,0 +1,498 @@
+"""Execute trades using Velvet vault and Enso."""
+import datetime
+import os
+from decimal import Decimal
+
+import pytest
+
+from tradeexecutor.ethereum.velvet.execution import VelvetExecution
+from tradeexecutor.ethereum.velvet.vault import VelvetVaultSyncModel
+from tradeexecutor.ethereum.velvet.velvet_enso_routing import VelvetEnsoRouting
+from tradeexecutor.strategy.generic.generic_pricing_model import GenericPricing
+from tradingstrategy.chain import ChainId
+
+from tradeexecutor.state.state import State
+from tradeexecutor.strategy.pandas_trader.position_manager import PositionManager
+from tradeexecutor.strategy.trading_strategy_universe import TradingStrategyUniverse
+
+
+#: Detect Github Actions
+CI = os.environ.get("CI", None) is not None
+
+
+pytestmark = pytest.mark.skipif(
+    not os.environ.get("VELVET_VAULT_OWNER_PRIVATE_KEY"),
+    reason="Need to set VELVET_VAULT_OWNER_PRIVATE_KEY to a specific private key to run this test"
+)
+
+
+@pytest.fixture()
+def state_with_starting_positions(
+    velvet_test_vault_strategy_universe,
+    velvet_pricing_model,
+    base_example_vault,
+    base_usdc,
+) -> State:
+    """Scan the initial positions for the tests."""
+
+    strategy_universe = velvet_test_vault_strategy_universe
+    pricing_model = velvet_pricing_model
+
+    sync_model = VelvetVaultSyncModel(
+        vault=base_example_vault,
+        hot_wallet=None,
+    )
+
+    state = State()
+    portfolio = state.portfolio
+
+    # Get the initial positions
+    sync_model.sync_initial(
+        state,
+        reserve_asset=base_usdc,
+        reserve_token_price=1.0,
+    )
+    sync_model.sync_treasury(
+        datetime.datetime.utcnow(),
+        state,
+        supported_reserves=state.portfolio.get_reserve_assets(),
+    )
+    sync_model.sync_positions(
+        datetime.datetime.utcnow(),
+        state,
+        strategy_universe=strategy_universe,
+        pricing_model=pricing_model,
+    )
+
+    # We have DogInMe position + cash
+    assert len(portfolio.open_positions) == 1
+    assert len(state.portfolio.get_reserve_assets()) == 1
+    assert state.portfolio.get_cash() == pytest.approx(2.674828)
+    assert portfolio.open_positions[1].get_quantity() == pytest.approx(Decimal(580.917745152826802993))
+
+    return state
+
+
+def test_keycat_weth_price(
+    velvet_test_vault_strategy_universe: TradingStrategyUniverse,
+    velvet_pricing_model: GenericPricing,
+):
+    """See we correctly quote KEYCAT/WETH pair price in USDC.
+
+    - We want all prices quoted in dollars
+    """
+    strategy_universe = velvet_test_vault_strategy_universe
+    pricing_model = velvet_pricing_model
+
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v2", "KEYCAT", "WETH"))
+    weth = pair.quote
+
+    weth_price = pricing_model.get_exchange_rate(
+        datetime.datetime.utcnow(),
+        weth,
+    )
+
+    assert 2000 < weth_price < 10_000
+
+    price = pricing_model.get_mid_price(datetime.datetime.utcnow(), pair)
+    assert 0.001 < price < 0.3
+
+    price_structure = pricing_model.get_buy_price(datetime.datetime.utcnow(), pair, Decimal(1.0))
+    assert 0.001 < price_structure.price < 0.3
+
+    price_structure = pricing_model.get_sell_price(datetime.datetime.utcnow(), pair, Decimal(1.0))
+    assert 0.001 < price_structure.price < 0.3
+
+
+@pytest.mark.skipif(CI, reason="Skipped on continuous integration due to Velvet/Enso stability issues")
+def test_velvet_intent_based_open_position_uniswap_v2(
+    state_with_starting_positions: State,
+    velvet_test_vault_strategy_universe: TradingStrategyUniverse,
+    velvet_pricing_model: GenericPricing,
+    velvet_execution_model: VelvetExecution,
+    velvet_routing_model: VelvetEnsoRouting,
+):
+    """Perform an intent-based swap
+
+    - Do initial deposit scan. Capture the initial USDC and DogInMe in the vault.
+
+    - Prepare a trade execution on KEYCAT Uniswap v2
+
+    - Execute it
+
+    - See we got new tokens
+    """
+
+    state = state_with_starting_positions
+    strategy_universe = velvet_test_vault_strategy_universe
+    execution_model = velvet_execution_model
+    routing_model = velvet_routing_model
+
+    position_manager = PositionManager(
+        datetime.datetime.utcnow(),
+        universe=velvet_test_vault_strategy_universe,
+        state=state,
+        pricing_model=velvet_pricing_model,
+        default_slippage_tolerance=0.20,
+    )
+
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v2", "KEYCAT", "WETH"))
+
+    trades = position_manager.open_spot(
+        pair=pair,
+        value=1.0  # USDC
+    )
+    t = trades[0]
+
+    assert t.planned_reserve == pytest.approx(Decimal(1.0))
+
+    # Setup routing state for the approvals of this cycle
+    routing_state_details = execution_model.get_routing_state_details()
+    routing_state = routing_model.create_routing_state(strategy_universe, routing_state_details)
+
+    execution_model.initialize()
+
+    execution_model.execute_trades(
+        datetime.datetime.utcnow(),
+        state,
+        trades,
+        routing_model,
+        routing_state,
+        check_balances=True,
+    )
+
+    assert t.is_success(), f"Enso trade failed: {t.blockchain_transactions[0].revert_reason}"
+    assert 0 < t.executed_price < 1
+    assert t.executed_quantity > 10  # 129 as writing of this
+    assert t.executed_reserve == pytest.approx(Decimal(1))
+
+
+@pytest.mark.skipif(CI, reason="Skipped on continuous integration due to Velvet/Enso stability issues")
+def test_velvet_intent_based_reduce_position_uniswap_v3(
+    state_with_starting_positions: State,
+    velvet_test_vault_strategy_universe: TradingStrategyUniverse,
+    velvet_pricing_model: GenericPricing,
+    velvet_execution_model: VelvetExecution,
+    velvet_routing_model: VelvetEnsoRouting,
+):
+    """Perform an intent-based swap
+
+    - Do initial deposit scan. Capture the initial USDC and DogInMe in the vault.
+
+    - Prepare sell existing DogInMe position partially
+
+    - Execute it
+    """
+
+    state = state_with_starting_positions
+    strategy_universe = velvet_test_vault_strategy_universe
+    execution_model = velvet_execution_model
+    routing_model = velvet_routing_model
+
+    position_manager = PositionManager(
+        datetime.datetime.utcnow(),
+        universe=velvet_test_vault_strategy_universe,
+        state=state,
+        pricing_model=velvet_pricing_model,
+        default_slippage_tolerance=0.20,
+    )
+
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v3", "DogInMe", "WETH"))
+    position = position_manager.get_current_position_for_pair(pair)
+    quantity_to_reduce = position.get_quantity() / Decimal(2)
+    dollar_delta = position.get_value() * float(quantity_to_reduce / position.get_quantity())
+    trades = position_manager.adjust_position(
+        pair=pair,
+        dollar_delta=-dollar_delta,
+        quantity_delta=-quantity_to_reduce,
+        weight=1,
+    )
+    t = trades[0]
+    assert t.is_sell()
+    assert 0 < t.planned_reserve < 1
+    assert t.planned_quantity == pytest.approx(Decimal(-290.458872576413401496))
+    # Setup routing state for the approvals of this cycle
+    routing_state_details = execution_model.get_routing_state_details()
+    routing_state = routing_model.create_routing_state(strategy_universe, routing_state_details)
+
+    execution_model.initialize()
+
+    execution_model.execute_trades(
+        datetime.datetime.utcnow(),
+        state,
+        trades,
+        routing_model,
+        routing_state,
+        check_balances=True,
+    )
+
+    assert t.is_success(), f"Enso trade failed: {t.blockchain_transactions[0].revert_reason}"
+    assert 0 < t.executed_price < 1
+    assert t.executed_quantity == pytest.approx(Decimal('-290.458872576413401496'))
+    assert t.executed_reserve > 0
+
+
+@pytest.mark.skipif(CI, reason="Skipped on continuous integration due to Velvet/Enso stability issues")
+def test_velvet_intent_based_close_position_uniswap_v3(
+    state_with_starting_positions: State,
+    velvet_test_vault_strategy_universe: TradingStrategyUniverse,
+    velvet_pricing_model: GenericPricing,
+    velvet_execution_model: VelvetExecution,
+    velvet_routing_model: VelvetEnsoRouting,
+):
+    """Perform an intent-based swap
+
+    - Close exiting DogInMe positio fully
+    """
+
+    state = state_with_starting_positions
+    strategy_universe = velvet_test_vault_strategy_universe
+    execution_model = velvet_execution_model
+    routing_model = velvet_routing_model
+
+    position_manager = PositionManager(
+        datetime.datetime.utcnow(),
+        universe=velvet_test_vault_strategy_universe,
+        state=state,
+        pricing_model=velvet_pricing_model,
+        default_slippage_tolerance=0.20,
+    )
+
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v3", "DogInMe", "WETH"))
+    position = position_manager.get_current_position_for_pair(pair)
+    trades = position_manager.close_position(position)
+    t = trades[0]
+    assert t.is_sell()
+    assert 0 < t.planned_reserve < 1
+    routing_state_details = execution_model.get_routing_state_details()
+    routing_state = routing_model.create_routing_state(strategy_universe, routing_state_details)
+
+    execution_model.initialize()
+
+    execution_model.execute_trades(
+        datetime.datetime.utcnow(),
+        state,
+        trades,
+        routing_model,
+        routing_state,
+        check_balances=True,
+    )
+
+    assert t.is_success(), f"Enso trade failed: {t.blockchain_transactions[0].revert_reason}"
+    assert 0 < t.executed_price < 1
+    assert t.executed_quantity == pytest.approx(Decimal('-580.917745152826802993'))
+    assert t.executed_reserve > 0
+
+    assert len(state.portfolio.closed_positions) == 1
+
+
+@pytest.mark.skipif(CI, reason="Skipped on continuous integration due to Velvet/Enso stability issues")
+def test_velvet_intent_based_increase_position_uniswap_v3(
+    state_with_starting_positions: State,
+    velvet_test_vault_strategy_universe: TradingStrategyUniverse,
+    velvet_pricing_model: GenericPricing,
+    velvet_execution_model: VelvetExecution,
+    velvet_routing_model: VelvetEnsoRouting,
+):
+    """Perform an intent-based swap
+
+    - Do initial deposit scan. Capture the initial USDC and DogInMe in the vault.
+
+    - Prepare sell existing DogInMe position partially
+
+    - Execute it
+    """
+
+    state = state_with_starting_positions
+    strategy_universe = velvet_test_vault_strategy_universe
+    execution_model = velvet_execution_model
+    routing_model = velvet_routing_model
+
+    position_manager = PositionManager(
+        datetime.datetime.utcnow(),
+        universe=velvet_test_vault_strategy_universe,
+        state=state,
+        pricing_model=velvet_pricing_model,
+        default_slippage_tolerance=0.25,
+    )
+
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v3", "DogInMe", "WETH"))
+    dollar_delta = 0.1  # Buy 0.1 more on the existing position
+    position = position_manager.get_current_position_for_pair(pair)
+    quantity_delta = dollar_delta * position.get_current_price()
+    trades = position_manager.adjust_position(
+        pair=pair,
+        dollar_delta=dollar_delta,
+        quantity_delta=quantity_delta,
+        weight=1,
+    )
+    t = trades[0]
+    assert t.is_buy()
+    assert 0 < t.planned_reserve < 1
+    assert t.planned_quantity > 100
+    # Setup routing state for the approvals of this cycle
+    routing_state_details = execution_model.get_routing_state_details()
+    routing_state = routing_model.create_routing_state(strategy_universe, routing_state_details)
+
+    execution_model.initialize()
+
+    execution_model.execute_trades(
+        datetime.datetime.utcnow(),
+        state,
+        trades,
+        routing_model,
+        routing_state,
+        check_balances=True,
+    )
+
+    assert t.is_success(), f"Enso trade failed: {t.blockchain_transactions[0].revert_reason}"
+    assert 0 < t.executed_price < 1
+    assert t.executed_quantity > 100
+    assert t.executed_reserve > 0
+
+
+def test_velvet_intent_based_open_position_many(
+    state_with_starting_positions: State,
+    velvet_test_vault_strategy_universe: TradingStrategyUniverse,
+    velvet_pricing_model: GenericPricing,
+    velvet_execution_model: VelvetExecution,
+    velvet_routing_model: VelvetEnsoRouting,
+):
+    """Open multiple positions in one go.
+
+    - Try to buy multiple tokens once
+    """
+
+    state = state_with_starting_positions
+    strategy_universe = velvet_test_vault_strategy_universe
+    execution_model = velvet_execution_model
+    routing_model = velvet_routing_model
+
+    position_manager = PositionManager(
+        datetime.datetime.utcnow(),
+        universe=velvet_test_vault_strategy_universe,
+        state=state,
+        pricing_model=velvet_pricing_model,
+        default_slippage_tolerance=0.20,
+    )
+
+    trades = []
+
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v2", "KEYCAT", "WETH"))
+    trades += position_manager.open_spot(
+        pair=pair,
+        value=1.0  # USDC
+    )
+
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v3", "WETH", "USDC"))
+    trades += position_manager.open_spot(
+        pair=pair,
+        value=1.0  # USDC
+    )
+
+    # Setup routing state for the approvals of this cycle
+    routing_state_details = execution_model.get_routing_state_details()
+    routing_state = routing_model.create_routing_state(strategy_universe, routing_state_details)
+
+    execution_model.initialize()
+
+    execution_model.execute_trades(
+        datetime.datetime.utcnow(),
+        state,
+        trades,
+        routing_model,
+        routing_state,
+        check_balances=True,
+    )
+
+    for t in trades:
+        assert t.is_success(), f"Enso trade failed: {t.blockchain_transactions[0].revert_reason}: {t}"
+        assert 0 < t.executed_price < 5000
+        assert t.executed_quantity > 0.0001
+
+    assert all(t for t in trades if t.is_success())
+
+
+def test_velvet_intent_based_open_many_close_one(
+    state_with_starting_positions: State,
+    velvet_test_vault_strategy_universe: TradingStrategyUniverse,
+    velvet_pricing_model: GenericPricing,
+    velvet_execution_model: VelvetExecution,
+    velvet_routing_model: VelvetEnsoRouting,
+):
+    """Open multiple positions in one go.
+
+    - Open two positions
+
+    - Close one fully
+    """
+
+    state = state_with_starting_positions
+    strategy_universe = velvet_test_vault_strategy_universe
+    execution_model = velvet_execution_model
+    routing_model = velvet_routing_model
+
+    position_manager = PositionManager(
+        datetime.datetime.utcnow(),
+        universe=velvet_test_vault_strategy_universe,
+        state=state,
+        pricing_model=velvet_pricing_model,
+        default_slippage_tolerance=0.20,
+    )
+
+    trades = []
+
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v2", "KEYCAT", "WETH"))
+    trades += position_manager.open_spot(
+        pair=pair,
+        value=1.0  # USDC
+    )
+
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v3", "WETH", "USDC"))
+    trades += position_manager.open_spot(
+        pair=pair,
+        value=1.0  # USDC
+    )
+
+    # Setup routing state for the approvals of this cycle
+    routing_state_details = execution_model.get_routing_state_details()
+    routing_state = routing_model.create_routing_state(strategy_universe, routing_state_details)
+
+    execution_model.initialize()
+
+    execution_model.execute_trades(
+        datetime.datetime.utcnow(),
+        state,
+        trades,
+        routing_model,
+        routing_state,
+        check_balances=True,
+    )
+
+    for t in trades:
+        assert t.is_success(), f"Enso trade failed: {t.blockchain_transactions[0].revert_reason}: {t}"
+        assert 0 < t.executed_price < 5000
+        assert t.executed_quantity > 0.0001
+
+    assert all(t for t in trades if t.is_success())
+
+    # Now close WETH
+    trades = []
+    pair = strategy_universe.get_pair_by_human_description((ChainId.base, "uniswap-v3", "WETH", "USDC"))
+    position = position_manager.get_current_position_for_pair(pair)
+    trades += position_manager.close_position(position)
+    execution_model.execute_trades(
+        datetime.datetime.utcnow(),
+        state,
+        trades,
+        routing_model,
+        routing_state,
+        check_balances=True,
+    )
+
+    for t in trades:
+        assert t.is_success(), f"Enso trade failed: {t.blockchain_transactions[0].revert_reason}: {t}"
+    assert all(t for t in trades if t.is_success())
+
+    # Keycat, Sky
+    assert len(state.portfolio.open_positions) == 2
