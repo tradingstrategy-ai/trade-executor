bbd7ebed1a66b8d44e559f7929f6b660b48173e7
Mikko Ohtamaa <mikko@redinnovation.com>
2025-01-05 21:22:12 +0100

    Initial Lagoon sync works

--- tests/lagoon/test_lagoon_deposit.py
+++ tests/lagoon/test_lagoon_deposit.py
@@ -1,38 +1,31 @@
 """Test Velvet vault deposits are correctly read."""
 import datetime
+from decimal import Decimal
 
 import pytest
 from eth_typing import HexAddress
-from web3 import Web3
 
-from eth_defi.lagoon.vault import LagoonVault
+from eth_defi.lagoon.deployment import LagoonAutomatedDeployment
 from eth_defi.token import TokenDetails
 from eth_defi.trace import assert_transaction_success_with_explanation
-from eth_defi.velvet import VelvetVault
-from tradeexecutor.ethereum.lagoon.vault import LagoonVaultSyncModel
 
-from tradeexecutor.ethereum.velvet.vault import VelvetVaultSyncModel
-from tradeexecutor.state.identifier import AssetIdentifier
+from tradeexecutor.ethereum.lagoon.vault import LagoonVaultSyncModel
 from tradeexecutor.state.portfolio import ReserveMissing
 from tradeexecutor.state.state import State
-from tradeexecutor.strategy.pricing_model import PricingModel
 from tradeexecutor.strategy.trading_strategy_universe import TradingStrategyUniverse
-from tradingstrategy.chain import ChainId
 
 
 def test_lagoon_treasury_initialise(
-    lagoon_vault: LagoonVault,
-    base_usdc: AssetIdentifier,
+    automated_lagoon_vault: LagoonAutomatedDeployment,
+    vault_strategy_universe: TradingStrategyUniverse,
 ):
-    """Initialise Velvet treasury
+    """Initialise the treasury"""
 
-    - Do initial deposit scan.
-
-    - Capture the initial USDC in the vault as a treasury
-    """
+    vault = automated_lagoon_vault.vault
+    asset_usdc = vault_strategy_universe.get_reserve_asset()
 
     sync_model = LagoonVaultSyncModel(
-        vault=lagoon_vault,
+        vault=vault,
         hot_wallet=None,
     )
 
@@ -46,7 +39,7 @@ def test_lagoon_treasury_initialise(
 
     sync_model.sync_initial(
         state,
-        reserve_asset=base_usdc,
+        reserve_asset=asset_usdc,
         reserve_token_price=1.0,
     )
     assert len(portfolio.reserves) == 1  # USDC added as the reserve asset
@@ -61,17 +54,54 @@ def test_lagoon_treasury_initialise(
     cycle = datetime.datetime.utcnow()
     events = sync_model.sync_treasury(cycle, state)
     assert len(events) == 0
-    assert treasury.last_block_scanned > 0
-    assert treasury.last_updated_at is not None
-    assert treasury.last_cycle_at is not None
+    assert treasury.last_block_scanned is None
+    assert treasury.last_updated_at is None
+    assert treasury.last_cycle_at is None
     assert len(treasury.balance_update_refs) == 0
     assert len(reserve_position.balance_updates) == 0
 
+
+def test_lagoon_deposit(
+    automated_lagoon_vault: LagoonAutomatedDeployment,
+    base_usdc: TokenDetails,
+    vault_strategy_universe: TradingStrategyUniverse,
+    depositor: HexAddress,
+):
+    """Do the initial lagoon deposit."""
+
+    vault = automated_lagoon_vault.vault
+    usdc = base_usdc
+    strategy_universe = vault_strategy_universe
+
+    sync_model = LagoonVaultSyncModel(
+        vault=vault,
+        hot_wallet=None,
+    )
+
+    state = State()
+    usdc_asset = strategy_universe.get_reserve_asset()
+    sync_model.sync_initial(
+        state,
+        reserve_asset=usdc_asset,
+        reserve_token_price=1.0,
+    )
+
     # Do initial deposit
+    # Deposit 9.00 USDC into the vault from the first user
+    usdc_amount = Decimal(9.00)
+    raw_usdc_amount = usdc.convert_to_raw(usdc_amount)
+    tx_hash = usdc.approve(vault.address, usdc_amount).transact({"from": depositor})
+    assert_transaction_success_with_explanation(web3, tx_hash)
+    deposit_func = vault.request_deposit(depositor, raw_usdc_amount)
+    tx_hash = deposit_func.transact({"from": depositor})
+    assert_transaction_success_with_explanation(web3, tx_hash)
+    assert usdc.fetch_balance_of(vault.silo_address) == pytest.approx(Decimal(9))
 
     # Process the initial deposits
     cycle = datetime.datetime.utcnow()
     events = sync_model.sync_treasury(cycle, state)
+    treasury = state.sync.treasury
+    reserve_position = state.portfolio.get_default_reserve_position()
     assert len(events) == 1
     assert treasury.last_block_scanned > 0
     assert treasury.last_updated_at is not None
@@ -86,99 +116,5 @@ def test_lagoon_treasury_initialise(
     assert len(reserve_position.balance_updates) == 1
 
     # Check we account USDC correctly
-    assert portfolio.get_cash() == pytest.approx(2.674828)
-
-
-@pytest.mark.skip(reason="Velvet is broken")
-def test_velvet_sync_deposit(
-    web3: Web3,
-    base_example_vault: VelvetVault,
-    base_usdc: AssetIdentifier,
-    base_doginme: AssetIdentifier,
-    deposit_user,
-        base_usdc_address: HexAddress,
-    velvet_test_vault_strategy_universe: TradingStrategyUniverse,
-    velvet_test_vault_pricing_model: PricingModel,
-    base_usdc_token: TokenDetails,
-):
-    """Sync Velvet deposit"""
-
-    vault = base_example_vault
-    strategy_universe = velvet_test_vault_strategy_universe
-    pricing_model = velvet_test_vault_pricing_model
-
-    sync_model = VelvetVaultSyncModel(
-        vault=vault,
-        hot_wallet=None,
-    )
-
-    state = State()
     portfolio = state.portfolio
-
-    sync_model.sync_initial(
-        state,
-        reserve_asset=base_usdc,
-        reserve_token_price=1.0,
-    )
-    assert len(portfolio.reserves) == 1  # USDC added as the reserve asset
-
-    pair = strategy_universe.get_pair_by_human_description(
-        (ChainId.base, "uniswap-v3", "DogInMe", "WETH")
-    )
-    assert pair is not None
-
-    # Process the initial deposits
-    cycle = datetime.datetime.utcnow()
-    events = sync_model.sync_treasury(cycle, state)
-    assert len(events) == 1
-    assert portfolio.get_cash() == pytest.approx(2.674828)
-
-    events = sync_model.sync_positions(cycle, state, strategy_universe, pricing_model)
-    assert len(events) == 0  # TODO: There is no event generated yet because we magically open spot position
-    dog_in_me_position_size = portfolio.get_position_by_trading_pair(pair).get_value()
-    assert dog_in_me_position_size > 0  # Value floats by the day
-
-    #
-    # Process additional deposits
-    #
-
-    # Velvet deposit manager on Base,
-    # the destination of allowance
-    deposit_manager = "0xe4e23120a38c4348D7e22Ab23976Fa0c4Bf6e2ED"
-
-    # Check there is ready-made manual approve() waiting onchain
-    allowance = base_usdc_token.contract.functions.allowance(
-        Web3.to_checksum_address(deposit_user),
-        Web3.to_checksum_address(deposit_manager),
-        ).call()
-    assert allowance == 5 * 10**6
-
-    tx_data = sync_model.vault.prepare_deposit_with_enso(
-        from_=deposit_user,
-        deposit_token_address=base_usdc_address,
-        amount=4 * 10**6,
-    )
-    tx_hash = web3.eth.send_transaction(tx_data)
-    assert_transaction_success_with_explanation(web3, tx_hash)
-
-    #
-    # Sync updated balances
-    #
-
-    # Process the initial deposits
-    cycle = datetime.datetime.utcnow()
-    events = sync_model.sync_treasury(cycle, state)
-    assert len(events) == 1
-    assert portfolio.get_cash() == pytest.approx(7.023765)
-
-    # DogInMe balance has increased after the deposit
-    #
-    assert portfolio.get_position_by_trading_pair(pair).get_value() > dog_in_me_position_size
-
-    #
-    # Check empty deposit cycle yields no events
-    #
-    cycle = datetime.datetime.utcnow()
-    events = sync_model.sync_treasury(cycle, state)
-    assert len(events) == 0
-    assert portfolio.get_cash() == pytest.approx(7.023765)
+    assert portfolio.get_cash() == pytest.approx(9)
