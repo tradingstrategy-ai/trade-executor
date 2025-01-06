c837bd57cdf425b462be1c3ddf39f7c7abfaf7ab
Mikko Ohtamaa <mikko@redinnovation.com>
2025-01-06 21:23:35 +0100

    Add missing v

--- tradeexecutor/cli/commands/lagoon_deploy_vault.py
+++ tradeexecutor/cli/commands/lagoon_deploy_vault.py
@@ -49,7 +49,13 @@ from eth_defi.deploy import deploy_contract
 from eth_defi.enzyme.deployment import POLYGON_DEPLOYMENT, EnzymeDeployment, ETHEREUM_DEPLOYMENT
 from eth_defi.enzyme.generic_adapter_vault import deploy_vault_with_generic_adapter
 from eth_defi.hotwallet import HotWallet
-from eth_defi.token import fetch_erc20_details
+from eth_defi.lagoon.deployment import LagoonDeploymentParameters, deploy_automated_lagoon_vault
+from eth_defi.token import fetch_erc20_details, USDC_NATIVE_TOKEN
+from eth_defi.uniswap_v2.constants import UNISWAP_V2_DEPLOYMENTS
+from eth_defi.uniswap_v2.deployment import fetch_deployment
+from tests.lagoon.test_deploy_base import multisig_owners
+
+from tradeexecutor.cli.commands.shared_options import parse_comma_separated_list
 from tradeexecutor.cli.guard import get_enzyme_deployment, generate_whitelist
 from tradeexecutor.monkeypatch.web3 import construct_sign_and_send_raw_middleware
 from tradingstrategy.chain import ChainId
@@ -76,27 +82,26 @@ def enzyme_deploy_vault(
     vault_record_file: Optional[Path] = Option(..., envvar="VAULT_RECORD_FILE", help="Store vault and comptroller addresses in this JSON file. It's important to write down all contract addresses."),
     fund_name: Optional[str] = Option(..., envvar="FUND_NAME", help="On-chain name for the fund shares"),
     fund_symbol: Optional[str] = Option(..., envvar="FUND_SYMBOL", help="On-chain token symbol for the fund shares"),
-    comptroller_lib: Optional[str] = shared_options.comptroller_lib,
     denomination_asset: Optional[str] = Option(None, envvar="DENOMINATION_ASSET", help="Stablecoin asset used for vault denomination"),
-
-    owner_address: Optional[str] = Option(None, envvar="OWNER_ADDRESS", help="The protocol or multisig address that is set as the owner of the vault"),
-    terms_of_service_address: Optional[str] = Option(None, envvar="TERMS_OF_SERVICE_ADDRESS", help="The address of the terms of service smart contract"),
+    multisig_owners: Optional[list[str]] = Option(None, callback=parse_comma_separated_list, envvar="MULTISIG_OWNERS", help="The list of acconts that are set to the cosigners of the Safe. The multisig threshold is number of cosigners - 1."),
+    # terms_of_service_address: Optional[str] = Option(None, envvar="TERMS_OF_SERVICE_ADDRESS", help="The address of the terms of service smart contract"),
     whitelisted_assets: Optional[str] = Option(..., envvar="WHITELISTED_ASSETS", help="Space separarted list of ERC-20 addresses this vault can trade. Denomination asset does not need to be whitelisted separately."),
     any_asset: Optional[bool] = Option(False, envvar="ANY_ASSET", help="Allow trading of any ERC-20 on Uniswap (unsecure)."),
 
     unit_testing: bool = shared_options.unit_testing,
-    production: bool = Option(False, envvar="PRODUCTION", help="Set production metadata flag true for the deployment."),
+    # production: bool = Option(False, envvar="PRODUCTION", help="Set production metadata flag true for the deployment."),
     simulate: bool = Option(False, envvar="SIMULATE", help="Simulate deployment using Anvil mainnet work, when doing manual deployment testing."),
     etherscan_api_key: Optional[str] = Option(None, envvar="ETHERSCAN_API_KEY", help="Etherscan API key need to verify the contracts on a production deployment."),
     one_delta: bool = Option(False, envvar="ONE_DELTA", help="Whitelist 1delta interaction with GuardV0 smart contract."),
     aave: bool = Option(False, envvar="AAVE", help="Whitelist Aave aUSDC deposits"),
+    uniswap_v2: bool = Option(False, envvar="AAVE", help="Whitelist Uniswap v2"),
+    uniswap_v3: bool = Option(False, envvar="AAVE", help="Whitelist Uniswap v3"),
 ):
-    """Deploy a new Enzyme vault.
+    """Deploy a new Lagoon vault.
 
-    Deploys a new Enzyme vault that is configured to be run with Trading Strategy Protocol.
+    Deploys a new Lagoon vault, Safe and TradingStrategyModuleV0 for automated trading.
 
-    Multiple contracts will be deployed and verified in a blockchain explorer.
-    The vault is configured with custom guard, deposit and terms of service contracts.
+    TODO: Heavily under development.
     """
 
     assert any_asset, "Currently only any_asset configurations supported"
@@ -141,10 +146,8 @@ def enzyme_deploy_vault(
     logger.info(f"  Chain id is {web3.eth.chain_id:,}")
     logger.info(f"  Latest block is {web3.eth.block_number:,}")
 
-    asset_manager_address = hot_wallet.address
-
-    if owner_address is None:
-        owner_address = hot_wallet.address
+    # TODO: Asset manager is now always the deployer
+    asset_manager = hot_wallet.address
 
     if simulate:
         logger.info("Simulation deployment")
@@ -153,13 +156,9 @@ def enzyme_deploy_vault(
     logger.info("-" * 80)
     logger.info("Deployer hot wallet: %s", hot_wallet.address)
     logger.info("Deployer balance: %f, nonce %d", hot_wallet.get_native_currency_balance(web3), hot_wallet.current_nonce)
-    logger.info("Enzyme FundDeployer: %s", enzyme_deployment.contracts.fund_deployer.address)
-    if enzyme_deployment.usdc is not None:
-        logger.info("USDC: %s", enzyme_deployment.usdc.address)
-    logger.info("Terms of service: %s", terms_of_service.address if terms_of_service else "-")
     logger.info("Fund: %s (%s)", fund_name, fund_symbol)
+    logger.info("Whitelisting any token: %s", aave)
     logger.info("Whitelisted assets: %s", ", ".join([a.symbol for a in whitelisted_asset_details]))
-
     logger.info("Whitelisting 1delta: %s", one_delta)
     logger.info("Whitelisting Aave: %s", aave)
 
@@ -168,13 +167,8 @@ def enzyme_deploy_vault(
     else:
         logger.error("Etherscan API key missing")
 
-    if owner_address != hot_wallet.address:
-        logger.info("Ownership will be transferred to %s", owner_address)
-    else:
-        logger.warning("Ownership will be retained at the deployer %s", hot_wallet.address)
-
-    if asset_manager_address != hot_wallet.address:
-        logger.info("Asset manager is %s", asset_manager_address)
+    if asset_manager != hot_wallet.address:
+        logger.info("Asset manager is %s", asset_manager)
     else:
         logger.info("Hot wallet set for the asset manager role")
 
@@ -191,77 +185,50 @@ def enzyme_deploy_vault(
             print("Aborted")
             sys.exit(1)
 
-    # TODO: Hack
-    if chain_id == ChainId.arbitrum:
-        uniswap_v2 = False
-    else:
-        uniswap_v2 = True
+    # Currently assumes HotWallet = asset manager
+    # as the trade-executor that deploys the vault is going to
+    # the assset manager for this vault
+    parameters = LagoonDeploymentParameters(
+        underlying=USDC_NATIVE_TOKEN[chain_id],
+        name="Example",
+        symbol="EXA",
+    )
 
-    try:
-        # Currently assumes HotWallet = asset manager
-        # as the trade-executor that deploys the vault is going to
-        # the assset manager for this vault
-        vault = deploy_vault_with_generic_adapter(
-            enzyme_deployment,
-            hot_wallet,
-            asset_manager=hot_wallet.address,
-            owner=owner_address,
-            terms_of_service=terms_of_service,
-            denomination_asset=denomination_token.contract,
-            fund_name=fund_name,
-            fund_symbol=fund_symbol,
-            whitelisted_assets=whitelisted_asset_details,
-            etherscan_api_key=etherscan_api_key if not simulate else None,  # Only verify when not simulating
-            production=production,
-            one_delta=one_delta,
-            aave=aave,
-            uniswap_v2=uniswap_v2,
+    if uniswap_v2:
+        chain_slug = chain_id.get_slug()
+        uniswap_v2_deployment = fetch_deployment(
+            web3,
+            factory_address=UNISWAP_V2_DEPLOYMENTS[chain_slug]["factory"],
+            router_address=UNISWAP_V2_DEPLOYMENTS[chain_slug]["router"],
+            init_code_hash=UNISWAP_V2_DEPLOYMENTS[chain_slug]["init_code_hash"],
         )
+    else:
+        uniswap_v2_deployment = None
 
-    except Exception as e:
+    assert not uniswap_v3, "Not implemented"
 
-        logger.error("Failed to deploy, is_mainnet_fork(): %s", web3config.is_mainnet_fork())
-
-        if web3config.is_mainnet_fork():
-            # Try to get some useful debug info from Anvil
-            web3config.close(logging.ERROR)
-
-        logger.exception(e)  # The fancy traceback formatter does not do nested
-
-        raise RuntimeError(f"Deployment failed. Hot wallet: {hot_wallet.address}.\nException: {e}") from e
+    deploy_info = deploy_automated_lagoon_vault(
+        web3=web3,
+        deployer=hot_wallet.account,
+        asset_manager=asset_manager,
+        parameters=parameters,
+        safe_owners=multisig_owners,
+        safe_threshold=len(multisig_owners) - 1,
+        uniswap_v2=uniswap_v2_deployment,
+        uniswap_v3=None,
+        any_asset=True,
+    )
 
     if vault_record_file and (not simulate):
         # Make a small file, mostly used to communicate with unit tests
         with open(vault_record_file, "wt") as out:
-            vault_record = {
-                "fund_name": fund_name,
-                "fund_symbol": fund_symbol,
-                "vault": vault.address,
-                "comptroller": vault.comptroller.address,
-                "generic_adapter": vault.generic_adapter.address,
-                "block_number": vault.deployed_at_block,
-                "usdc_payment_forwarder": vault.payment_forwarder.address,
-                "guard": vault.guard_contract.address,
-                "deployer": hot_wallet.address,
-                "denomination_token": denomination_token.address,
-                "terms_of_service": terms_of_service_address,
-                "whitelisted_assets": whitelisted_assets,
-                "asset_manager_address": asset_manager_address,
-                "owner_address": owner_address,
-            }
+            vault_record = deploy_info.pformat()
             json.dump(vault_record, out, indent=4)
         logger.info("Wrote %s for vault details", os.path.abspath(vault_record_file))
     else:
         logger.info("Skipping record file because of simulation")
 
-    buf = StringIO()
-    for key, value in vault.get_deployment_info().items():
-        print(f"{key}={value}", file=buf)
-
-    logger.info(
-        "Vault environment variables for trade-executor init command:\n%s",
-        buf.getvalue()
-    )
+    logger.info("Lagoon deployed:\n%s", deploy_info.pformat())
 
     web3config.close()
 
