fb5996fc4b90de9c0de0a1501288ddc2f46a5995
Mikko Ohtamaa <mikko@redinnovation.com>
2025-02-28 09:43:32 +0200

    Cleaning up test

--- tests/backtest/test_min_tvl_universe.py
+++ tests/backtest/test_min_tvl_universe.py
@@ -36,42 +36,9 @@ class Parameters:
     chain_id = ChainId.base
     exchanges = {"uniswap-v2", "uniswap-v3"}
 
-    #
-    # Basket construction and rebalance parameters
-    #
-    min_asset_universe = 1  # How many assets we need in the asset universe to start running the index
-    max_assets_in_portfolio = 10  # How many assets our basket can hold once
-    allocation = 0.95  # Allocate all cash to volatile pairs
-    # min_rebalance_trade_threshold_pct = 0.05  # % of portfolio composition must change before triggering rebalacne
-    individual_rebalance_min_threshold_usd = 5.0  # Don't make buys less than this amount
-    per_position_cap_of_pool = 0.01  # Never own more than % of the lit liquidity of the trading pool
-    max_concentration = 0.20  # How large % can one asset be in a portfolio once
-    min_portfolio_weight = 0.0050  # Close position / do not open if weight is less than 50 BPS
-
-    # ATH indicator parameters
-    ath_delay_bars = 144
-    ath_window_bars = 360
-    ath_threshold = 1.10
-    ath_span = 72
-
-    # RSI filter parameters
-    daily_rsi_bars = 90
-    daily_rsi_threshold = 25
-
-    # For the length of trailing sharpe used in inclusion criteria
-    rolling_volume_bars = pd.Timedelta("7d") // candle_time_bucket.to_timedelta()
-    rolling_volatility_bars = pd.Timedelta("7d") // candle_time_bucket.to_timedelta()
-    tvl_ewm_span = 7 * 24  # Smooth TVL inclusin criteria
-    min_volume = 25_000  # USD
     min_tvl_prefilter = 1_500_000  # USD - to reduce number of trading pairs for backtest-purposes only
     min_tvl = 1_500_000  # USD - set to same as above if you want to avoid any survivorship bias
-    min_token_sniffer_score = 20  # 20 = AAVE
-
-    #
-    # Yield on cash
-    #
-    use_aave = True
-    credit_flow_dust_threshold = 5.0  # Min deposit USD to Aave
+    min_token_sniffer_score = 50
 
     #
     #
@@ -80,15 +47,6 @@ class Parameters:
     #
     backtest_start = datetime.datetime(2024, 1, 1)
     backtest_end = datetime.datetime(2024, 2, 4)
-    initial_cash = 100_000
-
-    #
-    # Live only
-    #
-    routing = TradeRouting.default
-    required_history_period = datetime.timedelta(days=2 * 14 + 1)
-    slippage_tolerance = 0.0060  # 0.6%
-    assummed_liquidity_when_data_missings = 10_000
 
 
 
@@ -138,156 +96,11 @@ def create_trading_universe(
     # TODO: PandasPairUniverse(buidl_index=True) - speed this up by skipping index building
     all_pairs_df = client.fetch_pair_universe().to_pandas()
     all_pairs_df = filter_for_exchange_slugs(all_pairs_df, Parameters.exchanges)
-    print("Creating universe for benchmark pair extraction")
     pair_universe = PandasPairUniverse(
         all_pairs_df,
         exchange_universe=exchange_universe,
         build_index=False,
     )
-    print(f"Exchanges {Parameters.exchanges} have total {len(all_pairs_df):,} pairs on chain {Parameters.chain_id.get_name()}")
-
-    #
-    # Do exchange and TVL prefilter pass for the trading universe
-    #
-    logging.getLogger().setLevel(logging.INFO)
-    tvl_df = client.fetch_tvl(
-        mode="min_tvl",
-        bucket=TimeBucket.d1,
-        start_time=Parameters.backtest_start,
-        end_time=Parameters.backtest_end,
-        exchange_ids=[exc.exchange_id for exc in targeted_exchanges],
-        min_tvl=Parameters.min_tvl_prefilter,
-    )
-    logging.getLogger().setLevel(logging.WARNING)
-
-    tvl_filtered_pair_ids = tvl_df["pair_id"].unique()
-    benchmark_pair_ids = [pair_universe.get_pair_by_human_description(desc).pair_id for desc in SUPPORTING_PAIRS]
-    needed_pair_ids = set(benchmark_pair_ids) | set(tvl_filtered_pair_ids)
-    pairs_df = all_pairs_df[all_pairs_df["pair_id"].isin(needed_pair_ids)]
-    print(f"After TVL prefilter to {Parameters.min_tvl_prefilter:,} in {Parameters.backtest_start} - {Parameters.backtest_end}, we have {len(pairs_df)} trading pairs")
-
-    category_df = pairs_df
-    category_df = add_base_quote_address_columns(category_df)
-    category_df = filter_for_stablecoins(category_df, StablecoinFilteringMode.only_volatile_pairs)
-    category_df = filter_for_derivatives(category_df)
-
-    allowed_quotes = {
-        PREFERRED_STABLECOIN,
-        WRAPPED_NATIVE_TOKEN[chain_id.value].lower(),
-    }
-
-    category_df = filter_for_quote_tokens(category_df, allowed_quotes)
-    category_pair_ids = category_df["pair_id"]
-    our_pair_ids = list(category_pair_ids) + benchmark_pair_ids
-    print(f"After remaining filtering we have {len(our_pair_ids)} pairs")
-    pairs_df = category_df[category_df["pair_id"].isin(our_pair_ids)]
-
-    # Never deduplicate supporting pars
-    supporting_pairs_df = pairs_df[pairs_df["pair_id"].isin(benchmark_pair_ids)]
-
-    # Deduplicate trading pairs - Choose the best pair with the best volume
-    deduplicated_df = deduplicate_pairs_by_volume(pairs_df)
-    pairs_df = pd.concat([deduplicated_df, supporting_pairs_df]).drop_duplicates(subset='pair_id', keep='first')
-    print(f"After deduplication we have {len(pairs_df)} pairs")
-
-    # Add benchmark pairs back to the dataset
-    pairs_df = pd.concat([pairs_df, supporting_pairs_df]).drop_duplicates(subset='pair_id', keep='first')
-
-    # Load metadata
-    print("Loading metadata")
-    logging.getLogger().setLevel(logging.INFO)
-    pairs_df = load_token_metadata(pairs_df, client)
-    logging.getLogger().setLevel(logging.WARNING)
-
-    # Scam filter using TokenSniffer
-    risk_filtered_pairs_df = filter_by_token_sniffer_score(
-        pairs_df,
-        risk_score=Parameters.min_token_sniffer_score,
-    )
-
-    # Check if we accidentally get rid of benchmark pairs we need for the strategy
-    difference = set(benchmark_pair_ids).difference(set(risk_filtered_pairs_df["pair_id"]))
-    if difference:
-        first_dropped_id = next(iter(difference))
-        first_dropped_data = pairs_df.loc[pairs_df.pair_id == first_dropped_id]
-        assert len(first_dropped_data) == 1, f"Got {len(first_dropped_data)} entries: {first_dropped_data}"
-        raise AssertionError(f"Benchmark trading pair dropped in filter_by_token_sniffer_score() check: {first_dropped_data.iloc[0]}")
-    pairs_df = risk_filtered_pairs_df.sort_values("volume", ascending=False)
-    print(f"After TokenSniffer risk filter we have {len(pairs_df)} pairs")
-
-    uni_v2 = pairs_df.loc[pairs_df["exchange_slug"] == "uniswap-v2"]
-    uni_v3 = pairs_df.loc[pairs_df["exchange_slug"] == "uniswap-v3"]
-    other_dex = pairs_df.loc[~((pairs_df["exchange_slug"] != "uniswap-v3") | (pairs_df["exchange_slug"] != "uniswap-v2"))]
-    print(f"Pairs on Uniswap v2: {len(uni_v2)}, Uniswap v3: {len(uni_v3)}, other DEX: {len(other_dex)}")
-    dataset = load_partial_data(
-        client=client,
-        time_bucket=Parameters.candle_time_bucket,
-        pairs=pairs_df,
-        execution_context=execution_context,
-        universe_options=universe_options,
-        liquidity=True,
-        liquidity_time_bucket=TimeBucket.d1,
-        liquidity_query_type=OHLCVCandleType.tvl_v2,
-        lending_reserves=LENDING_RESERVES,
-    )
-
-    reserve_asset = PREFERRED_STABLECOIN
-
-    print("Creating trading universe")
-    strategy_universe = TradingStrategyUniverse.create_from_dataset(
-        dataset,
-        reserve_asset=reserve_asset,
-        forward_fill=True,  # We got very gappy data from low liquid DEX coins
-    )
-
-    # Tag benchmark/routing pairs tokens so they can be separated from the rest of the tokens
-    # for the index construction.
-    strategy_universe.warm_up_data()
-    for pair_id in benchmark_pair_ids:
-        pair = strategy_universe.get_pair_by_id(pair_id)
-        pair.other_data["benchmark"] = False
-
-    print(f"Total {strategy_universe.get_pair_count()}")
-
-    return strategy_universe
-
-
-def create_trading_universe(
-    timestamp: datetime.datetime,
-    client: Client,
-    execution_context: ExecutionContext,
-    universe_options: UniverseOptions,
-) -> TradingStrategyUniverse:
-    """Create the trading universe.
-
-    - Load Trading Strategy full pairs dataset
-
-    - Load built-in Coingecko top 1000 dataset
-
-    - Get all DEX tokens for a certain Coigecko category
-
-    - Load OHCLV data for these pairs
-
-    - Load also BTC and ETH price data to be used as a benchmark
-    """
-
-    chain_id = Parameters.chain_id
-
-    exchange_universe = client.fetch_exchange_universe()
-    targeted_exchanges = [exchange_universe.get_by_chain_and_slug(ChainId.base, slug) for slug in Parameters.exchanges]
-
-    # Pull out our benchmark pairs ids.
-    # We need to construct pair universe object for the symbolic lookup.
-    # TODO: PandasPairUniverse(buidl_index=True) - speed this up by skipping index building
-    all_pairs_df = client.fetch_pair_universe().to_pandas()
-    all_pairs_df = filter_for_exchange_slugs(all_pairs_df, Parameters.exchanges)
-    print("Creating universe for benchmark pair extraction")
-    pair_universe = PandasPairUniverse(
-        all_pairs_df,
-        exchange_universe=exchange_universe,
-        build_index=False,
-    )
-    print(f"Exchanges {Parameters.exchanges} have total {len(all_pairs_df):,} pairs on chain {Parameters.chain_id.get_name()}")
 
     #
     # Do exchange and TVL prefilter pass for the trading universe
@@ -305,7 +118,6 @@ def create_trading_universe(
     benchmark_pair_ids = [pair_universe.get_pair_by_human_description(desc).pair_id for desc in SUPPORTING_PAIRS]
     needed_pair_ids = set(benchmark_pair_ids) | set(tvl_filtered_pair_ids)
     pairs_df = all_pairs_df[all_pairs_df["pair_id"].isin(needed_pair_ids)]
-    print(f"After TVL prefilter to {Parameters.min_tvl_prefilter:,} in {Parameters.backtest_start} - {Parameters.backtest_end}, we have {len(pairs_df)} trading pairs")
 
     category_df = pairs_df
     category_df = add_base_quote_address_columns(category_df)
@@ -320,7 +132,6 @@ def create_trading_universe(
     category_df = filter_for_quote_tokens(category_df, allowed_quotes)
     category_pair_ids = category_df["pair_id"]
     our_pair_ids = list(category_pair_ids) + benchmark_pair_ids
-    print(f"After remaining filtering we have {len(our_pair_ids)} pairs")
     pairs_df = category_df[category_df["pair_id"].isin(our_pair_ids)]
 
     # Never deduplicate supporting pars
@@ -329,13 +140,11 @@ def create_trading_universe(
     # Deduplicate trading pairs - Choose the best pair with the best volume
     deduplicated_df = deduplicate_pairs_by_volume(pairs_df)
     pairs_df = pd.concat([deduplicated_df, supporting_pairs_df]).drop_duplicates(subset='pair_id', keep='first')
-    print(f"After deduplication we have {len(pairs_df)} pairs")
 
     # Add benchmark pairs back to the dataset
     pairs_df = pd.concat([pairs_df, supporting_pairs_df]).drop_duplicates(subset='pair_id', keep='first')
 
     # Load metadata
-    print("Loading metadata")
     pairs_df = load_token_metadata(pairs_df, client)
 
     # Scam filter using TokenSniffer
@@ -352,12 +161,7 @@ def create_trading_universe(
         assert len(first_dropped_data) == 1, f"Got {len(first_dropped_data)} entries: {first_dropped_data}"
         raise AssertionError(f"Benchmark trading pair dropped in filter_by_token_sniffer_score() check: {first_dropped_data.iloc[0]}")
     pairs_df = risk_filtered_pairs_df.sort_values("volume", ascending=False)
-    print(f"After TokenSniffer risk filter we have {len(pairs_df)} pairs")
 
-    uni_v2 = pairs_df.loc[pairs_df["exchange_slug"] == "uniswap-v2"]
-    uni_v3 = pairs_df.loc[pairs_df["exchange_slug"] == "uniswap-v3"]
-    other_dex = pairs_df.loc[~((pairs_df["exchange_slug"] != "uniswap-v3") | (pairs_df["exchange_slug"] != "uniswap-v2"))]
-    print(f"Pairs on Uniswap v2: {len(uni_v2)}, Uniswap v3: {len(uni_v3)}, other DEX: {len(other_dex)}")
     dataset = load_partial_data(
         client=client,
         time_bucket=Parameters.candle_time_bucket,
@@ -370,7 +174,6 @@ def create_trading_universe(
 
     reserve_asset = PREFERRED_STABLECOIN
 
-    print("Creating trading universe")
     strategy_universe = TradingStrategyUniverse.create_from_dataset(
         dataset,
         reserve_asset=reserve_asset,
@@ -383,8 +186,6 @@ def create_trading_universe(
     for pair_id in benchmark_pair_ids:
         pair = strategy_universe.get_pair_by_id(pair_id)
         pair.other_data["benchmark"] = False
-
-    print(f"Total {strategy_universe.get_pair_count()}")
 
     return strategy_universe
 
