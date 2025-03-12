7003147a50d9c45b3f856d618b64d9e42e85e0e7
Mikko Ohtamaa <mikko@redinnovation.com>
2025-03-12 20:31:15 +0200

    Shorten 1h decision cycle

--- tradeexecutor/backtest/preprocessed_backtest.py
+++ tradeexecutor/backtest/preprocessed_backtest.py
@@ -192,7 +192,7 @@ def run_and_write_report(
         # Run the notebook
         universe_size = os.path.getsize(universe_path)
         dataset_size = os.path.getsize(dataset_path)
-        logger.info(f"Starting backtest dataset notebook execution, dataset size is {dataset_size:,}b, universe size is {universe_size:,}b")
+        logger.info(f"Starting backtest {dataset.set.slug}, dataset notebook execution, dataset size is {dataset_size:,}b, universe size is {universe_size:,}b")
         ep = ExecutePreprocessor(timeout=3600, kernel_name='python3')
 
         try:
