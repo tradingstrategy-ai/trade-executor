eeb9c6514215304384da433c61d6e7caddaf2748
Mikko Ohtamaa <mikko@redinnovation.com>
2025-03-24 18:35:31 +0200

    Token cache purge (#1159)
    - Add `token-cache` CLI command for inspecting and purging the token cache

---
+++ tests/cli/test_cli_purge_cache.py
@@ -0,0 +1,32 @@
+"""Command token-cache unit testst"""
+import os.path
+from contextlib import redirect_stdout
+from io import StringIO
+
+
+
+from tradeexecutor.cli.main import app
+
+def test_cli_purge_token_cache(mocker, persistent_test_client):
+    """token-cache command purging missing entries."""
+
+    client = persistent_test_client
+
+    environment = {
+        "CACHE_PATH": str(client.transport.get_abs_cache_path()),
+        "PURGE_TYPE": "missing_tokensniffer_data",
+        "PRINT_TOKENS": "all",
+        "UNIT_TESTING": "true",
+        "TRADING_STRATEGY_API_KEY": os.environ["TRADING_STRATEGY_API_KEY"],
+    }
+
+    mocker.patch.dict("os.environ", environment, clear=True)
+
+    f = StringIO()
+    with redirect_stdout(f):
+        app(["token-cache"], standalone_mode=False)
+
+    # print(f.getvalue())
+    assert "count" in f.getvalue()
+
+
---
+++ tradeexecutor/cli/commands/token_cache.py
@@ -0,0 +1,89 @@
+"""Print out token cache status of the client."""
+import enum
+import logging
+from pathlib import Path
+from typing import Optional
+
+from tabulate import tabulate
+from typer import Option
+
+from tradingstrategy.client import Client
+from tradingstrategy.transport.token_cache import read_token_cache, calculate_token_cache_summary, display_token_metadata
+from . import shared_options
+from .app import app
+from .shared_options import required_option
+
+logger = logging.getLogger(__name__)
+
+
+class PurgeType(enum.Enum):
+    none = "none"
+    all = "all"
+    missing_tokensniffer_data = "missing_tokensniffer_data"
+
+
+class PrintTokenOption(enum.Enum):
+    none = "none"
+    all = "all"
+
+
+@app.command()
+def token_cache(
+    cache_path: Optional[Path] = shared_options.cache_path,
+    purge: PurgeType = Option("none", envvar="PURGE_TYPE", help="Which cache entries to purge"),
+    print_option: PrintTokenOption = Option("none", envvar="PRINT_TOKENS", help="Which token metadata to print"),
+    unit_testing: bool = shared_options.unit_testing,
+    trading_strategy_api_key: str = required_option(shared_options.trading_strategy_api_key),
+):
+    """Display and purge token cache entries.
+
+    - Token metadata cache contains data from TokenSniffer and CoinGecko APIs that may be stale
+    """
+
+    client = Client.create_live_client(
+        api_key=trading_strategy_api_key,
+        cache_path=cache_path,  # Set via environment variable for unit testing
+        settings_path=None,  # No interactive settings file with live execution
+    )
+
+    cached_entries = list(read_token_cache(client.transport))
+
+    summary = calculate_token_cache_summary(cached_entries)
+
+    table = [[key, value] for key, value in summary.items()]
+
+    path = client.transport.get_abs_cache_path() / 'token-metadata'
+    print(f"Cache path: {path}")
+    print("Token metadata cache contents:")
+    print(tabulate(table, headers=["Key", "Value"], tablefmt="fancy_grid"))
+
+    match purge:
+        case PurgeType.none:
+            purge_entries = []
+        case PurgeType.missing_tokensniffer_data:
+            purge_entries = [e for e in cached_entries if not e.has_tokensniffer_data()]
+        case PurgeType.all:
+            purge_entries = cached_entries
+        case _:
+            raise NotImplementedError(f"PurgeType {purge} not implemented")
+
+    if print_option == PrintTokenOption.all:
+        # Print tokens
+        if len(cached_entries) == 0:
+            print("No data")
+        else:
+            print("Cache contents:")
+        data = display_token_metadata(cached_entries)
+        print(tabulate(data, headers="keys", tablefmt="fancy_grid"))
+
+    if purge_entries:
+        if not unit_testing:
+            resp = input(f"Do you want to purge {len(purge_entries)} tokens from cache? [y/N]")
+        else:
+            resp = "y"
+        if resp == "y":
+            for e in purge_entries:
+                print(f"Deleting {e.path}")
+                e.purge()
+    else:
+        print("Nothing to purge")
