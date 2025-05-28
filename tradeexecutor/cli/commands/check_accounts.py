0474c4eb454fdce8ad38e8038b07bcfa546fe78a
Mikko Ohtamaa <mikko@redinnovation.com>
2025-05-28 11:43:42 +0200

    Remove pdb

--- tradeexecutor/cli/commands/check_accounts.py
+++ tradeexecutor/cli/commands/check_accounts.py
@@ -189,7 +189,6 @@ def check_accounts(
 
     if clean:
         logger.info("All accounts match:\n%s", output)
-        import ipdb ; ipdb.set_trace()
         if not raise_on_unclean:
             sys.exit(0)
         else:
