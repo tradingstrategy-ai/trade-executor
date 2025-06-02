50b6ed61871f88fd38167311c411104c6a4d807c
Mikko Ohtamaa <mikko@redinnovation.com>
2025-06-02 21:39:50 +0200

    Add DataTooOld test

--- tests/backtest/test_forward_fill.py
+++ tests/backtest/test_forward_fill.py
@@ -6,8 +6,8 @@ import pandas as pd
 import pytest
 
 from tradeexecutor.strategy.execution_context import unit_test_execution_context
-from tradeexecutor.strategy.trading_strategy_universe import load_partial_data, TradingStrategyUniverse
-from tradeexecutor.strategy.universe_model import UniverseOptions
+from tradeexecutor.strategy.trading_strategy_universe import load_partial_data, TradingStrategyUniverse, TradingStrategyUniverseModel
+from tradeexecutor.strategy.universe_model import UniverseOptions, DataTooOld
 from tradingstrategy.chain import ChainId
 from tradingstrategy.client import Client
 from tradingstrategy.pair import PandasPairUniverse
@@ -164,3 +164,58 @@ def test_forward_fill_spot_only_gapped(persistent_test_client: Client):
     time_range = strategy_universe.data_universe.candles.get_timestamp_range(exclude_forward_fill=True)
     assert time_range == (pd.Timestamp('2022-01-01 00:00:00'), pd.Timestamp('2024-03-15 00:00:00'))
 
+
+
+def test_forward_fill_too_old(persistent_test_client: Client):
+    """Data too old warning triggers on forward filled data.
+
+    - Data is forward-filled until today, but should be still too old
+      as we should ignore the forward-filled part
+    """
+    client = persistent_test_client
+
+    # The pairs we are rading
+    pair_ids = [
+        (ChainId.polygon, "quickswap", "WBTC", "WETH", 0.0030),  # This data has a lot of gaps
+        (ChainId.polygon, "uniswap-v3", "WMATIC", "USDC", 0.0005),
+    ]
+
+    start_at = datetime.datetime(2024, 1, 1)
+    end_at = datetime.datetime(2024, 3, 15)
+
+    universe_options = UniverseOptions(
+        start_at=start_at,
+        end_at=end_at,
+    )
+
+    # Load all trading and lending data on Polygon
+    # for all lending markets on a relevant time period
+    dataset = load_partial_data(
+        client=client,
+        time_bucket=TimeBucket.h1,
+        pairs=pair_ids,
+        execution_context=unit_test_execution_context,
+        liquidity=False,
+        stop_loss_time_bucket=None,
+        universe_options=universe_options,
+    )
+
+    # Token debug
+    pairs = PandasPairUniverse(dataset.pairs, exchange_universe=dataset.exchanges)
+    for t in pairs.iterate_tokens():
+        assert t.chain_id == ChainId.polygon
+
+    now_= datetime.datetime.utcnow()
+    strategy_universe = TradingStrategyUniverse.create_from_dataset(
+        dataset,
+        reserve_asset="0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+        forward_fill=True,
+        forward_fill_until=now_,
+    )
+
+    with pytest.raises(DataTooOld):
+        TradingStrategyUniverseModel.check_data_age(
+            ts=now_,
+            universe=strategy_universe,
+            best_before_duration=datetime.timedelta(days=30),
+        )
