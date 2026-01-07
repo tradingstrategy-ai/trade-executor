9060b9635855d9768b114d860ed21b7e6b8f9342
Mikko Ohtamaa <mikko@redinnovation.com>
2026-01-07 13:54:15 +0200

    Get rid of some old tests

--- tests/test_data_age.py
+++ tests/test_data_age.py
@@ -39,7 +39,8 @@ class DataAgeTestUniverseModel(TradingStrategyUniverseModel):
         return universe
 
 
-@pytest.mark.slow_test_group
+# @pytest.mark.slow_test_group
+@pytest.mark.skipped(reason="Live integration - need to rewrite this test")
 def test_data_fresh(persistent_test_client):
     """Fresh data passes our data check."""
     # d1 data is used by other tests and cached
@@ -50,7 +51,7 @@ def test_data_fresh(persistent_test_client):
     universe_model.check_data_age(ts, universe, best_before_duration)
 
 
-@pytest.mark.slow_test_group
+@pytest.mark.skipped(reason="Live integration - need to rewrite this test")
 def test_data_aged(persistent_test_client):
     """Aged data raises an exception."""
     universe_model = DataAgeTestUniverseModel(persistent_test_client, timed_task)
