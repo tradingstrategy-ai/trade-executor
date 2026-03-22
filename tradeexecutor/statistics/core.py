0b5c3ca0d8309ee921d8cd7e3b365ad8435edc9a
Mikko Ohtamaa <mikko@redinnovation.com>
2026-03-22 13:58:38 +0100

    fix: count frozen positions in nav

--- tradeexecutor/statistics/core.py
+++ tradeexecutor/statistics/core.py
@@ -158,12 +158,13 @@ def calculate_statistics(
     open_position_equity = portfolio.get_position_equity_and_loan_nav()
     free_cash = float(portfolio.get_cash())
     total_equity = open_position_equity + free_cash
+    net_asset_value = portfolio.get_net_asset_value()
 
     # May not available for non-live execution and non-vault strategies
     if treasury:
         share_count = treasury.share_count
         if share_count:
-            share_price_usd = total_equity / float(share_count)
+            share_price_usd = net_asset_value / float(share_count)
         else:
             share_price_usd = None
     else:
@@ -184,7 +185,7 @@ def calculate_statistics(
         pf_stats = PortfolioStatistics(
             calculated_at=clock,
             total_equity=total_equity,
-            net_asset_value=portfolio.get_net_asset_value(),
+            net_asset_value=net_asset_value,
             unrealised_profitability=float(profitability_series.iloc[-1] if len(profitability_series) > 0 else 0),
             free_cash=free_cash,
             open_position_count=len(portfolio.open_positions),
@@ -210,7 +211,7 @@ def calculate_statistics(
         pf_stats = PortfolioStatistics(
             calculated_at=clock,
             total_equity=total_equity,
-            net_asset_value=portfolio.get_net_asset_value(),
+            net_asset_value=net_asset_value,
             free_cash=free_cash,
             open_position_equity=open_position_equity,
             share_count=share_count,
